1e5be874fe224a0095fc1d9e50408d106c0e4d8d
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2008-05-05 12:13:10 +0000

    made a copy

---
+++ OpenScrape/branches/pdiff/DialogEdit.cpp
@@ -0,0 +1,48 @@
+// EditDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "DialogEdit.h"
+
+// CDlgEdit dialog
+IMPLEMENT_DYNAMIC(CDlgEdit, CDialog)
+
+CDlgEdit::CDlgEdit(CWnd* pParent /*=NULL*/)	: CDialog(CDlgEdit::IDD, pParent) 
+{
+}
+
+CDlgEdit::~CDlgEdit() 
+{
+}
+
+void CDlgEdit::DoDataExchange(CDataExchange* pDX) 
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDITENTRY, m_EditEntry);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgEdit, CDialog)
+	ON_BN_CLICKED(IDOK, &CDlgEdit::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CDlgEdit message handlers
+BOOL CDlgEdit::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+
+	SetWindowText(m_titletext.GetString());
+	m_EditEntry.SetWindowText(m_result.GetString());
+	m_EditEntry.SetSel(MAKEWORD(0,-1));
+	m_EditEntry.SetFocus();
+
+	return FALSE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CDlgEdit::OnBnClickedOk() 
+{
+	m_EditEntry.GetWindowText(m_result);
+	OnOK();
+}
