82653acb02f5155720b551f3b1bb136e26b2ac61
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2008-06-20 16:07:08 +0000

    Reorganizing repository.

---
+++ OpenHoldem/ICMCalculator.cpp
@@ -0,0 +1,427 @@
+/*
+	   Author	:	spud
+	   Date		:	2007-MAR-08
+
+      Notation: P(i,n)=probability that player i finishes in nth place
+      P(w|z)=probability that w occurs given that z occurs
+      I=i's stack
+      T=SUM[I]
+
+      P(i,1)=I/T
+      P(i,n)=SUM(j!=i)[P(j,1)*P(i,n|j,1)]
+
+      ICM assumes that P(i,n|j,1) = (P(i,n-1) in a game where j has been removed)
+*/
+
+#include "stdafx.h"
+
+#include "symbols.h"
+#include "global.h"
+#include "ICMCalculator.h"
+
+
+double P(int i, int n, double *s, int N)
+{
+    double p = 0.;
+
+    if (n==1)
+    {
+        double I = s[i];
+        double T = 0;
+
+        for (int k=0; k < N; k++) T+=s[k];
+
+        p = (T > 0.) ? I / T : 0.;
+    }
+    else
+    {
+        for (int j=0; j < N; j++)
+        {
+            if (j != i)
+            {
+                double *ss = (double *) calloc(N-1,sizeof(double));
+                int ii = (i > j) ? i-1 : i;
+                memcpy(ss, s, j * sizeof(double));
+                memcpy(ss + j, s + (j + 1), (N - j -1) * sizeof(double));
+
+                p += P(j, 1, s, N) * P(ii, n-1, ss, N-1);
+
+                free(ss);
+            }
+        }
+    }
+    return p;
+}
+
+ICM::ICM ()
+{
+}
+
+ICM::~ICM ()
+{
+}
+
+double ICM::ProcessQueryICM(const char* pquery, int *e)
+{
+    double		prizes[MAX_PLAYERS] = {0};
+    double		stacks[MAX_PLAYERS] = {0};
+    int			i, j;
+    int			mychair = (int) symbols.sym.userchair;
+    double		pot = symbols.sym.pot;
+    int			opponentsplayingbits = (int) symbols.sym.opponentsplayingbits;
+    int			nopponentsplaying = (int) symbols.sym.nopponentsplaying;
+    int			nplayersseated =  (int) symbols.sym.nplayersseated;
+    int			playersseatedbits =  (int) symbols.sym.playersseatedbits;
+
+    prizes[0] = global.preferences.icm_prize1;
+    prizes[1] = global.preferences.icm_prize2;
+    prizes[2] = global.preferences.icm_prize3;
+    prizes[3] = global.preferences.icm_prize4;
+
+    for (i = 0; i < MAX_PLAYERS; i++)
+    {
+        if ((playersseatedbits>>i)&1)
+        {
+            stacks[i] = symbols.stacks_at_hand_start[i] - symbols.sym.currentbet[i];
+            //ASSERT(stacks[i] >= 0.0001);
+        }
+    }
+
+    if (strncmp(pquery,"_fold",5)==0)
+    {
+        double win = pot / nopponentsplaying;
+
+        for (i = 0; i < MAX_PLAYERS; i++)
+        {
+            if ((opponentsplayingbits>>i)&1)
+            {
+                stacks[i] += win;
+            }
+        }
+    }
+
+    else if (strncmp(pquery,"_callwin",8)==0)
+    {
+        double call = symbols.sym.call;
+
+        if (stacks[mychair] < call)
+        {
+            double myTotalBet = symbols.sym.currentbet[mychair] + stacks[mychair];
+
+            for (i = 0; i < MAX_PLAYERS; i++)
+            {
+                if ((opponentsplayingbits>>i)&1 && myTotalBet < symbols.sym.currentbet[i])
+                {
+                    double extra = symbols.sym.currentbet[i] - myTotalBet;
+
+                    stacks[i] += extra;
+                    pot -= extra;
+                }
+            }
+        }
+        stacks[mychair] += pot;
+    }
+
+    else if (strncmp(pquery,"_calllose",9)==0)
+    {
+        double call = symbols.sym.call;
+        double mycall = min(call,stacks[mychair]);
+        double win = (pot + mycall) / nopponentsplaying;
+
+        stacks[mychair] -= mycall;
+
+        for (i = 0; i < MAX_PLAYERS; i++)
+        {
+            if ((opponentsplayingbits>>i)&1)
+            {
+                stacks[i] += win;
+            }
+        }
+    }
+
+    else if (strncmp(pquery,"_calltie",8)==0)
+    {
+        double call = symbols.sym.call;
+        double win = 0.;
+
+        if (stacks[mychair] < call)
+        {
+            double myTotalBet = symbols.sym.currentbet[mychair] + stacks[mychair];
+
+            for (i = 0; i < MAX_PLAYERS; i++)
+            {
+                if ((opponentsplayingbits>>i)&1 && myTotalBet<symbols.sym.currentbet[i])
+                {
+                    double extra = symbols.sym.currentbet[i] - myTotalBet;
+
+                    stacks[i] += extra;
+                    pot -= extra;
+                }
+            }
+        }
+        pot += min(call, stacks[mychair]);
+        win = pot / (nopponentsplaying +1);
+        stacks[mychair] += win;
+        for (i = 0; i < MAX_PLAYERS; i++)
+        {
+            if ((opponentsplayingbits>>i)&1)
+            {
+                stacks[i] += win;
+            }
+        }
+    }
+
+    else if (strncmp(pquery,"_alliwin",8)==0)
+    {
+        if (isdigit(pquery[8]))
+        {
+            //assume callers are n smallest stacks
+            bool callers[MAX_PLAYERS] = {0};
+            int ncallers = min(pquery[8]-'0', nopponentsplaying);
+
+            for (i = 0; i < ncallers; i++)
+            {
+                int jsmallest = -1;
+                double smalleststack = MAX_DOUBLE;
+
+                for (j = 0; j < MAX_PLAYERS; j++)
+                {
+                    if ((opponentsplayingbits>>j)&1)
+                    {
+                        if (!callers[j] && (symbols.sym.currentbet[j] + stacks[j]) < smalleststack)
+                        {
+                            smalleststack = symbols.sym.currentbet[j] + stacks[j];
+                            jsmallest = j;
+                        }
+                    }
+                }
+                if (jsmallest > -1)
+                {
+                    double oppCurrentBet = symbols.sym.currentbet[jsmallest];
+                    double myTotalBet = symbols.sym.currentbet[mychair] + stacks[mychair];
+                    double extra = ((oppCurrentBet + stacks[jsmallest]) > myTotalBet) ? myTotalBet - oppCurrentBet :stacks[jsmallest];
+                    stacks[jsmallest] -= extra;
+                    callers[jsmallest] = true;
+                    pot+=extra;
+                }
+            }
+            stacks[mychair]+=pot;
+        }
+        else
+        {
+            //assume only one particular caller
+            int oppChair = getChairFromDealPos(pquery + 8);
+
+            if (oppChair >= 0)
+            {
+                double oppCurrentBet = symbols.sym.currentbet[oppChair];
+                double myTotalBet = symbols.sym.currentbet[mychair] + stacks[mychair];
+                double extra = ((oppCurrentBet + stacks[oppChair]) > myTotalBet) ? myTotalBet - oppCurrentBet :stacks[oppChair];
+
+                stacks[oppChair] -= extra;
+                stacks[mychair]+= (pot + extra);
+            }
+        }
+    }
+
+    else if (strncmp(pquery,"_allilose",9)==0)
+    {
+        if (isdigit(pquery[9]))
+        {
+            //assume callers are n biggest stacks
+            int ncallers = min(pquery[9]-'0', nopponentsplaying);
+
+            if (ncallers > 0)
+            {
+                bool callers[MAX_PLAYERS] = {0};
+                int *biggest =(int *) calloc(ncallers, sizeof(int));
+                double *sidepots = (double *)calloc(ncallers, sizeof(double));
+                double mybet = 0.;
+
+                memset(sidepots,0, ncallers * sizeof(double));
+
+                for (i = 0; i < ncallers; i++)
+                {
+                    int jbiggest = -1;
+                    double biggeststack = 0.;
+
+                    for (j = 0; j < MAX_PLAYERS; j++)
+                    {
+                        if ((opponentsplayingbits>>j)&1)
+                        {
+                            if (!callers[j] && (symbols.sym.currentbet[j] + stacks[j]) > biggeststack)
+                            {
+                                biggeststack = symbols.sym.currentbet[j] + stacks[j];
+                                jbiggest = j;
+                            }
+                        }
+                    }
+                    biggest[i] = jbiggest;
+                    if (jbiggest > -1)
+                    {
+                        callers[jbiggest] = true;
+                        stacks[jbiggest] += symbols.sym.currentbet[jbiggest];
+                        pot -= symbols.sym.currentbet[jbiggest];
+                    }
+                }
+                stacks[mychair] += symbols.sym.currentbet[mychair];
+                pot -= symbols.sym.currentbet[mychair];
+                for (i = (ncallers -1); i>=0; i--)
+                {
+                    ASSERT(biggest[i] >= 0);
+                    sidepots[i] = min(stacks[biggest[i]], stacks[mychair]) - mybet;
+                    mybet += sidepots[i];
+                }
+                stacks[mychair] -= mybet;
+                sidepots[ncallers - 1] += pot;
+                for (i = 0; i<ncallers; i++)
+                {
+                    double win = sidepots[i] / (i + 1);
+
+                    for (j = 0; j<=i; j++)
+                    {
+                        ASSERT(biggest[j] >= 0);
+                        stacks[biggest[j]] += win;
+                    }
+                }
+                free(biggest);
+                free(sidepots);
+            }
+        }
+        else
+        {
+            //assume only one particular caller
+            int oppChair = getChairFromDealPos(pquery + 9);
+
+            if (oppChair >= 0)
+            {
+                double oppTotalBet = symbols.sym.currentbet[oppChair] + stacks[oppChair];
+                double myTotalBet = symbols.sym.currentbet[mychair] + stacks[mychair];
+                double extra = (oppTotalBet > myTotalBet) ? stacks[mychair] : oppTotalBet - symbols.sym.currentbet[mychair];
+
+                stacks[oppChair]+= (pot + extra);
+                stacks[mychair]-= extra;
+            }
+        }
+    }
+
+    else if (strncmp(pquery,"_allitie",8)==0)
+    {
+        //assume only one particular caller
+        int oppChair = getChairFromDealPos(pquery + 8);
+
+        if (oppChair >= 0)
+        {
+            stacks[oppChair]+= pot / 2;
+            stacks[mychair]+= pot / 2;
+        }
+    }
+
+    else
+    {
+        for (i = 0; i < MAX_PLAYERS; i++)
+        {
+            if ((playersseatedbits>>i)&1)
+            {
+                stacks[i] += symbols.sym.currentbet[i];
+            }
+        }
+    }
+
+    return EquityICM(stacks, prizes, MAX_PLAYERS, mychair);
+}
+
+double ICM::EquityICM(double *stacks, double *prizes, int playerNB, int player)
+{
+    double ICM = 0.;
+    int i = 0;
+
+    for (i = 0; i < playerNB; i++)
+    {
+        //printf("player %d  stack = %1.2f \n", i, stacks[i]);
+    }
+
+	// Degenerate case when the player's stack is zero.  Place
+	// him after all opponents who had stacks at the start of this
+	// hand.  This will not tie-break if two players bust on the
+	// same hand.
+	if (stacks[player] == 0.)
+	{
+		int place = 0;
+
+		for (i = 0; i < MAX_PLAYERS; i++)
+		{
+			if (((int) symbols.sym.opponentsseatedbits >> i) & 1)
+				place++;
+		}
+
+		return prizes[place];
+
+	}
+
+    i = 0;
+    while (i < playerNB && prizes[i] > 0.)
+    {
+        double p = P(player, i + 1, stacks, playerNB);
+
+        //printf("=> prob place %d = %1.4f \n", i + 1, p);
+        ICM += prizes[i] * p;
+        i++;
+    }
+
+    return ICM;
+}
+
+double ICM::GetPlayerCurrentBet(int pos)
+{
+    return symbols.sym.currentbet[pos];
+}
+
+int ICM::getChairFromDealPos(const char* pquery)
+{
+    int chair = -1;
+    int nplayersseated = (int) symbols.sym.nplayersseated;
+
+    if (nplayersseated > 0)
+    {
+        int playersseatedbits = (int) symbols.sym.playersseatedbits;
+        int dealPos = -1;
+
+        if (strcmp(pquery,"SB")==0)
+            dealPos = 1;
+        else if (strcmp(pquery,"BB")==0)
+            dealPos = 2;
+        else if (strcmp(pquery,"UTG")==0)
+            dealPos = 3;
+        else if (strcmp(pquery,"UTG1")==0)
+            dealPos = 4;
+        else if (strcmp(pquery,"UTG2")==0)
+            dealPos = 5;
+        else if (strcmp(pquery,"UTG3")==0)
+            dealPos = 6;
+        else if (strcmp(pquery,"UTG4")==0)
+            dealPos = 7;
+        else if (strcmp(pquery,"UTG5")==0)
+            dealPos = 8;
+        else if (strcmp(pquery,"UTG6")==0)
+            dealPos = 9;
+        else if (strcmp(pquery,"D")==0)
+            dealPos = 0;
+        else if (strcmp(pquery,"CO")==0)
+            dealPos = nplayersseated - 1;
+
+        if (dealPos >= 0)
+        {
+            chair = (int) symbols.sym.dealerchair;
+
+            while (dealPos > 0)
+            {
+                chair = (chair + 1) % MAX_PLAYERS;
+
+                if ((playersseatedbits>>chair)&1)
+                    dealPos--;
+            }
+        }
+    }
+    return chair;
+}
---
+++ OpenScrape/DialogEditFont.cpp
@@ -0,0 +1,272 @@
+// DialogEditFont.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include <math.h>
+
+#include "OpenScrapeDoc.h"
+#include "DialogEditFont.h"
+
+// CDlgEditFont dialog
+
+IMPLEMENT_DYNAMIC(CDlgEditFont, CDialog)
+
+CDlgEditFont::CDlgEditFont(CWnd* pParent /*=NULL*/)
+	: CDialog(CDlgEditFont::IDD, pParent)
+{
+
+}
+
+CDlgEditFont::~CDlgEditFont()
+{
+}
+
+void CDlgEditFont::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_CHARACTER, m_Character);
+	DDX_Control(pDX, IDC_TYPE, m_Type);
+	DDX_Control(pDX, IDC_CHARLIST, m_CharList);
+	DDX_Control(pDX, IDC_PIXELSEP, m_PixelSep);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgEditFont, CDialog)
+	ON_BN_CLICKED(IDOK, &CDlgEditFont::OnBnClickedOk)
+	ON_LBN_SELCHANGE(IDC_CHARLIST, &CDlgEditFont::OnLbnSelchangeCharlist)
+	ON_EN_KILLFOCUS(IDC_CHARACTER, &CDlgEditFont::OnEnKillfocusCharacter)
+	ON_CBN_KILLFOCUS(IDC_TYPE, &CDlgEditFont::OnEnKillfocusType)
+	ON_BN_CLICKED(IDC_DELETE, &CDlgEditFont::OnBnClickedDelete)
+	ON_BN_CLICKED(IDC_SORT, &CDlgEditFont::OnBnClickedSort)
+END_MESSAGE_MAP()
+
+
+// CDlgEditFont message handlers
+BOOL CDlgEditFont::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	CString		text;
+	int			i;
+
+	// Font
+	lf_fixed.lfWidth = 0;
+	lf_fixed.lfHeight = 8;
+	lf_fixed.lfEscapement = 0;
+	lf_fixed.lfOrientation = 0;
+	lf_fixed.lfItalic = 0;
+	lf_fixed.lfUnderline = 0;
+	lf_fixed.lfStrikeOut = 0;
+	lf_fixed.lfCharSet = 0;
+	lf_fixed.lfOutPrecision = 0;
+	lf_fixed.lfClipPrecision = 0;
+	lf_fixed.lfQuality = PROOF_QUALITY;
+	lf_fixed.lfPitchAndFamily = 0;
+	strcpy(lf_fixed.lfFaceName, "Courier New");
+	separation_font.CreateFontIndirect(&lf_fixed);
+	m_PixelSep.SetFont(&separation_font);
+
+	SetWindowText(titletext.GetString());
+
+	m_Type.AddString("Type 0");
+	m_Type.AddString("Type 1");
+	m_Type.AddString("Type 2");
+	m_Type.AddString("Type 3");
+	m_Type.SelectString(-1, type.GetString());
+	m_Type.SetWindowPos(NULL, 0, 0, 75, 300, SWP_NOMOVE | SWP_NOZORDER);
+
+	m_Character.SetWindowText(character.GetString());
+
+	for (i = 0; i < new_t$_recs->GetCount(); i++)
+	{
+		Stablemap_font &fontrec = new_t$_recs->ElementAt(i);
+		text.Format("%c", fontrec.ch);
+		m_CharList.AddString(text.GetString());
+	}
+
+	m_CharList.SetCurSel(0);
+	OnLbnSelchangeCharlist();
+	m_CharList.SetFocus();
+
+	return false;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CDlgEditFont::OnLbnSelchangeCharlist()
+{
+	int				j, x, bit, top;
+	CString			separation, text, charstr, type;
+	bool			character[MAX_CHAR_WIDTH][MAX_CHAR_HEIGHT] = { false };	
+
+	if (m_CharList.GetCurSel() != LB_ERR)
+	{
+		Stablemap_font &fontrec = new_t$_recs->ElementAt(m_CharList.GetCurSel());
+
+		// Get set bits
+		bit = 0;
+		for (j=0; j<fontrec.x_count; j++)
+			for (bit=0; bit<MAX_CHAR_HEIGHT; bit++)
+				character[j][bit] = (fontrec.x[j] & (int) pow((double) 2, (double) bit)) != 0;
+
+		// Find topmost line with a set pixel
+		for (j=MAX_CHAR_HEIGHT-1; j>=0; j--)
+		{
+			for (x=0; x<fontrec.x_count; x++)
+			{
+				if (character[x][j])
+				{
+					top = j;
+					x = fontrec.x_count + 1;
+					j = -1;
+				}
+			}
+		}
+
+		// Create string of set pixels
+		separation = "";
+		for (j=top; j>=0; j--)
+		{
+			for (x=0; x<fontrec.x_count; x++)
+			{
+				if (character[x][j])
+					separation.Append("X");
+				else
+					separation.Append(" ");
+			}
+			separation.Append("\r\n");
+		}
+
+		// Update controls
+		m_PixelSep.SetWindowText(separation);
+		type.Format("Type %d", fontrec.group);
+		m_Type.SelectString(-1, type);
+		charstr.Format("%c", fontrec.ch);
+		m_Character.SetWindowText(charstr.GetString());
+	}
+
+	else
+	{
+		m_PixelSep.SetWindowText("");
+		m_Type.SetCurSel(-1);
+		m_Character.SetWindowText("");
+	}
+
+}
+
+void CDlgEditFont::OnBnClickedOk()
+{
+	COpenScrapeDoc			*pDoc = COpenScrapeDoc::GetDocument();
+	CString					charstr;
+	CString					temp_type, temp_character;
+
+	m_Type.GetWindowText(temp_type);
+	m_Character.GetWindowText(temp_character);
+
+	m_Type.GetWindowText(type);
+	m_Character.GetWindowText(character);
+
+	OnOK();
+}
+
+void CDlgEditFont::OnEnKillfocusCharacter()
+{
+	CString					temp_character, text;
+	int						cur_sel = m_CharList.GetCurSel();
+
+	m_Character.GetWindowText(temp_character);
+
+	Stablemap_font &fontrec = new_t$_recs->ElementAt(cur_sel);
+
+	fontrec.ch = temp_character.GetString()[0];
+
+	text.Format("%c", fontrec.ch);
+	m_CharList.DeleteString(cur_sel);
+	m_CharList.InsertString(cur_sel, text.GetString());
+	m_CharList.SetCurSel(cur_sel);
+
+}
+
+void CDlgEditFont::OnEnKillfocusType()
+{
+	CString					temp_type, text;
+	int						cur_sel = m_CharList.GetCurSel();
+
+	m_Type.GetWindowText(temp_type);
+
+	Stablemap_font &fontrec = new_t$_recs->ElementAt(cur_sel);
+
+	fontrec.group = atoi(temp_type.Mid(5,1).GetString());
+}
+
+void CDlgEditFont::OnBnClickedDelete()
+{
+	int		sel = m_CharList.GetCurSel();
+
+	new_t$_recs->RemoveAt(sel);
+	m_CharList.DeleteString(sel);
+	
+	if (m_CharList.GetCount()>0)
+		m_CharList.SetCurSel(0);
+
+	OnLbnSelchangeCharlist();
+	m_CharList.SetFocus();
+}
+
+void CDlgEditFont::OnBnClickedSort()
+{
+	int				i, j, k;
+	Stablemap_font	temp;
+	CString			text;
+	int				size = (int) new_t$_recs->GetSize();
+
+	// Bubble sort em
+	for (i=0; i<size-1; i++)
+	{
+		for (j=i+1; j<size; j++)
+		{
+			Stablemap_font &i_fontrec = new_t$_recs->ElementAt(i);
+			Stablemap_font &j_fontrec = new_t$_recs->ElementAt(j);
+			if (j_fontrec.ch < i_fontrec.ch ||
+				j_fontrec.ch == i_fontrec.ch && j_fontrec.group < i_fontrec.group)
+			{
+				// hold i rec in temp
+				temp.ch = i_fontrec.ch;
+				temp.group = i_fontrec.group;
+				temp.x_count = i_fontrec.x_count;
+				for (k=0; k<i_fontrec.x_count; k++)
+					temp.x[k] = i_fontrec.x[k];
+				temp.hexmash = i_fontrec.hexmash;
+
+				// copy j rec to i rec
+				i_fontrec.ch = j_fontrec.ch;
+				i_fontrec.group = j_fontrec.group;
+				i_fontrec.x_count = j_fontrec.x_count;
+				for (k=0; k<j_fontrec.x_count; k++)
+					i_fontrec.x[k] = j_fontrec.x[k];
+				i_fontrec.hexmash = j_fontrec.hexmash;
+
+				// copy temp to j rec
+				j_fontrec.ch = temp.ch;
+				j_fontrec.group = temp.group;
+				j_fontrec.x_count = temp.x_count;
+				for (k=0; k<temp.x_count; k++)
+					j_fontrec.x[k] = temp.x[k];
+				j_fontrec.hexmash = temp.hexmash;
+			}
+		}
+	}
+
+	// Clear and re-populate list box
+	m_CharList.ResetContent();
+	for (i=0; i < size; i++)
+	{
+		Stablemap_font &fontrec = new_t$_recs->ElementAt(i);
+		text.Format("%c", fontrec.ch);
+		m_CharList.AddString(text.GetString());
+	}
+
+	// Select first item in list box
+	m_CharList.SetCurSel(0);
+	OnLbnSelchangeCharlist();
+	m_CharList.SetFocus();
+}
