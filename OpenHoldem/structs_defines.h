add0006f6049c86983fb8c71e88b6bad30b23200
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2008-07-18 19:08:54 +0000

    Updated release notes and version numbers for 2008-07-25 pre-release build 1.

--- OpenHoldem/structs_defines.h
+++ OpenHoldem/structs_defines.h
@@ -6,8 +6,8 @@
 
 typedef boost::spirit::node_val_data_factory<const char *> int_factory_t;
 
-#define VERSION_NUMBER				1.91
-#define VERSION_TEXT				"1.9.1"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER				1.92
+#define VERSION_TEXT				"1.9.2"  // change version number in OpenHoldem.rc also, when needed
 
 // thread timeout
 #define THREAD_WAIT					3000
