00269deeebc9a74d11e522eacbf92d70211c7f35
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2008-12-26 16:59:21 +0000

    Trunk: Fix for chip scraping bug identified by Ares, where chip scrape routine would not stop horizontal scanning on a non-match on the first vertical chip.

--- OpenHoldem/CScraper.cpp
+++ OpenHoldem/CScraper.cpp
@@ -2029,6 +2029,10 @@ const double CScraper::DoChipScrape(RMapCI r_iter)
 				// no hash match
 				if (h_iter == p_tablemap->h$(hash_type)->end())
 				{
+					// Stop horizontal scrape loop if chipindex==0 AND a non-match
+					if (chipindex==0)
+						stackindex = MAX_CHIP_STACKS+1;
+
 					// stop vertical scrape loop on a non-match
 					chipindex = MAX_CHIPS_PER_STACK+1;
 				}
