24e71fe45665eb901d0f46c1ea34fab26a99bbf7
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2009-03-24 17:00:07 +0000

    Applied Oscar's keyboard control patch for OpenScrape.

--- OpenScrape/OpenScrapeView.h
+++ OpenScrape/OpenScrapeView.h
@@ -11,6 +11,7 @@ protected: // create from serialization only
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 	afx_msg BOOL OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
 
 	CPen		black_pen, green_pen, red_pen, blue_pen, white_dot_pen, black_dot_pen, null_pen;
