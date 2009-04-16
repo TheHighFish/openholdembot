0ee8779f2f047858774936149cada5819b797ee0
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2009-04-16 20:09:47 +0000

    Fix for issue 7: "chipscrapemethod" now accepts a value of "XxY" which will trigger the check of X stacks of Y chips.

--- trunk/CTablemap/CTablemap.h
+++ trunk/CTablemap/CTablemap.h
@@ -184,11 +184,9 @@ public:
 									  else if (s.MakeLower() == "yes") return true; 
 									  else return false; }
 
-	const bool chipscrapemethod()	{ SMapCI it = _s$.find("chipscrapemethod"); 
-									  if (it==_s$.end()) return false;
-									  CString s=it->second.text;
-									  if (s.MakeLower() == "all") return true;
-									  else return false; }
+	const CString chipscrapemethod(){ SMapCI it = _s$.find("chipscrapemethod"); 
+									  if (it==_s$.end()) return "";
+									  else return it->second.text; }
 
 	const CString scraperdll()		{ SMapCI it = _s$.find("scraperdll"); if (it!=_s$.end()) return it->second.text; else return ""; }
 
