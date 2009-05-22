f7dc505d9662ccaf65d0e654161539f1973137b5
openholdem <openholdem@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2009-05-22 20:21:53 +0000

    Removing 2.0.0 PRB1 tag

--- tags/2009-04-01_2.0.0_PRB1/OpenHoldem/Validator_Rules/consistency_checks_history_inline.cpp
+++
@@ -1,9 +0,0 @@
-BEGIN_TESTCASE
-    TESTCASE_ID ("1100")
-    HEURISTIC_RULE (false)
-    REASONING ("If it's my turn, my previous action could have been neither allin nor fold.")
-    PRECONDITION (gws("ismyturn"))
-    POSTCONDITION ((gws("prevaction") != 4) && (gws("prevaction") != -1))
-    SYMBOLS_POSSIBLY_AFFECTED ("ismyturn, prevaction")
-END_TESTCASE
-
