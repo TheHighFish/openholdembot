0f6765cd355c36a73b6b560b2dcebe8e819e69da
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2009-11-01 19:28:43 +0000

    RC: Store TM connection data.

--- OpenHoldem_Reconnection_Development/CAutoConnector.h
+++ OpenHoldem_Reconnection_Development/CAutoConnector.h
@@ -4,6 +4,22 @@
 #include "..\CCritSec\CCritSec.h"
 #include "..\CTablemap\CTablemap.h"
 
+
+const int k_MaxNumberOfTableMaps = 25;
+
+typedef struct
+{
+	unsigned int	ClientSizeX, ClientSizeY;
+	unsigned int	ClientSizeMinX, ClientSizeMinY;
+	unsigned int	ClientSizeMaxX, ClientSizeMaxY;
+	CString			TitleText;
+	CString			TitleText_0_9[10];
+	CString			NegativeTitleText;
+	CString			NegativeTitleText_0_9[10];
+	STablemapRegion	TablePoint;
+} t_TablemapConnectionData;
+
+
 extern class CAutoConnector
 {
 public:
@@ -15,26 +31,28 @@ public:
 	double TimeSinceLastFailedAttemptToConnect(); // seconds
 public:
 	// public mutators
+	const HWND attached_hwnd() { return _attached_hwnd; }
 	#define ENT CSLock lock(m_critsec);
 	void set_attached_hwnd(const HWND h) { ENT _attached_hwnd = h; }
-#undef ENT
-	const HWND attached_hwnd() { return _attached_hwnd; }
+	#undef ENT
 private:
 	void ParseAllOpenScrapeOrWinScrapeTableMapsToLoadConnectionData(CString TableMapWildcard);
 	void ParseAllTableMapsToLoadConnectionData();
+	void ExtractConnectionDataFromCurrentTablemap(SWholeMap *map);
 private:
 	int SelectTableMapAndWindow(int Choices);
 	int SelectTableMapAndWindowManually(int Choices);
 	int SelectTableMapAndWindowAutomatically(int Choices);
 private:
 	void Check_TM_Against_All_Windows(SWholeMap smap, HWND targetHWnd);
-
 private:
 	// private variables - use public accessors and public mutators to address these
-	HWND			_attached_hwnd;	 // Table that we are attached to
-	bool			TablemapsInScraperFolderAlreadyParsed;
+	HWND		_attached_hwnd;	 // Table that we are attached to
+	bool		TablemapsInScraperFolderAlreadyParsed;
+	int			NumberOfTableMapsLoaded;
+	CCritSec	m_critsec;
 private:
-	CCritSec		m_critsec;
+	t_TablemapConnectionData	TablemapConnectionData[k_MaxNumberOfTableMaps];
 } *p_autoconnector;
 
 #endif INC_CAUTOCONNECTOR_H
