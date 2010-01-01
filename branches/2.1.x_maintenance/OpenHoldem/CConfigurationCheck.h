b436e4948191aed843ac7152dd5b087e26b4247d
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2010-01-01 03:54:23 +0000

    M: ConfigurationCheck

---
+++ branches/2.1.x_maintenance/OpenHoldem/CConfigurationCheck.h
@@ -0,0 +1,18 @@
+#ifndef INC_CCONFIGURATIONCHECK_H
+#define INC_CCONFIGURATIONCHECK_H
+
+extern class CConfigurationCheck
+{
+public:
+	CConfigurationCheck();
+	~CConfigurationCheck();
+private:
+	void CheckEverything();
+private:
+	void CheckKeyboardSettings();
+	void CheckColourDepth();
+	void CheckMicrosoftVisualCppRuntimeLibrary();
+	void CheckFontSmoothing();
+} *p_cconfigurationcheck;
+
+#endif // INC_CCONFIGURATIONCHECK_H
