b4ea242198d8596ff6e52623b6c504c92e7d728d
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2010-02-02 10:16:55 +0000

    Creating branch for frefactoring of $play

---
+++ trunk/OpenHoldem.f_play/MyCToolBar.h
@@ -0,0 +1,22 @@
+#ifndef INC_MYCTOOLBAR_H
+#define INC_MYCTOOLBAR_H
+
+
+// CMyToolBar
+
+class CMyToolBar : public CToolBar
+{
+	DECLARE_DYNAMIC(CMyToolBar)
+
+public:
+	CMyToolBar();
+	virtual ~CMyToolBar();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual void OnUpdateCmdUI(CFrameWnd* pTarget, BOOL bDisableIfNoHandler);
+};
+
+
+#endif //INC_MYCTOOLBAR_H
