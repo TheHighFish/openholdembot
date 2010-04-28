1b42a8fa8462ff1ea3d5eee5bd48b6bc5c8107e6
SpewPoker <SpewPoker@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2010-04-28 15:02:25 +0000

    Removal of Debug dialog boxes

--- OpenHoldem/CLazyScraper.cpp
+++ OpenHoldem/CLazyScraper.cpp
@@ -23,16 +23,13 @@ void CLazyScraper::DoScrape()
 	// of the heartbeat-cycle, we do optionally
 	// execute only partial scrapes (aka "Lazy Scraping").
 	// Please see the manual for more info about that.
-	MessageBox(0, "Basic", "Scraping", 0);
 	p_scraper->DoBasicScrapeButtons();
 	if (CardScrapeNeeded())
 	{
-		MessageBox(0, "Cards", "Scraping", 0);
 		write_log(3, "CLazyScraper: CardScrape needed\n");
 		p_scraper->DoBasicScrapeAllPlayerCards();
 		if (CompleteScrapeNeeded())
 		{
-			MessageBox(0, "Full", "Scraping", 0);
 			write_log(3, "CLazyScraper: CompleteScrape needed\n");
 			p_scraper->CompleteBasicScrapeToFullScrape();
 		}
