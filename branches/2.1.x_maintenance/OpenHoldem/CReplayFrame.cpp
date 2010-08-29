6b974b55cdbfa7413c996ec923f29d80b8300ca6
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2010-08-29 12:06:28 +0000

    M: Improved replay-frames.

--- branches/2.1.x_maintenance/OpenHoldem/CReplayFrame.cpp
+++ branches/2.1.x_maintenance/OpenHoldem/CReplayFrame.cpp
@@ -113,16 +113,18 @@ void CReplayFrame::CreateReplayFrame(void)
 		fprintf(fp, "<br>\n");
 
 		// Table title
-		fprintf(fp, "%s", p_scraper->title());
-		fprintf(fp, "\n");
-
-		// Links forwards and backwards to the next frames
-		fprintf(fp, "%s", GetLinksToPrevAndNextFile());
+		fprintf(fp, "[%s]", p_scraper->title());
+		fprintf(fp, "<br>\n");
 
 		// Session, frame number and time
 		fprintf(fp, " [Session %lu]", p_sessioncounter->session_id());
 		fprintf(fp, " [Frame: %06d]", _next_replay_frame);
 		fprintf(fp, " [%s]<br>\n", now_time_str);
+		fprintf(fp, "<br>\n");
+
+		// Links forwards and backwards to the next frames
+		fprintf(fp, "%s", LPCSTR(GetLinksToPrevAndNextFile()));
+		fprintf(fp, "<br>\n");
 		fprintf(fp, "<br>\n");
 
 		// Header of main table for smaller data tables
@@ -131,16 +133,20 @@ void CReplayFrame::CreateReplayFrame(void)
 		fprintf(fp, "<td>\n");
 
 		// Data tables
-		fprintf(fp, "%s", GetPlayerInfoAsHTML());
-		fprintf(fp, "%s", GetButtonStatesAsHTML());
-		fprintf(fp, "%s", GetBlindInfoAsHTML());
-		fprintf(fp, "%s", GetCommonCardsAsHTML());
-		fprintf(fp, "%s", GetPotsAsHTML());
+		fprintf(fp, "%s", LPCSTR(GetPlayerInfoAsHTML()));
+		fprintf(fp, "/<td>\n");
+		fprintf(fp, "<td>\n");
+		fprintf(fp, "%s", LPCSTR(GetButtonStatesAsHTML()));
+		fprintf(fp, "%s", LPCSTR(GetBlindInfoAsHTML()));
+		fprintf(fp, "%s", LPCSTR(GetCommonCardsAsHTML()));
+		fprintf(fp, "%s", LPCSTR(GetPotsAsHTML()));
+		fprintf(fp, "</td>\n");
 
 		// Footer of main table
-		fprintf(fp, "</td>\n");
 		fprintf(fp, "</tr>\n");
 		fprintf(fp, "</table>\n");
+
+		// End of HTML
 		fprintf(fp, "</body></html>\n");
 
 		fclose(fp);
@@ -199,51 +205,61 @@ CString CReplayFrame::GetPlayerInfoAsHTML()
 	// Table header for: SFABD (seated, friend, active, button, dealt, playing),
 	// hand, bet, balance, name
 	player_info += "<table border=4 cellpadding=1 cellspacing=1>\n";
-	player_info += "<tr>\n";
-	player_info += "<th>#</th>\n";
-	player_info += "<th>SFABDP</th>\n";  // seated, friend, active, button, dealt, playing
-	player_info += "<th>hand</th>\n";
-	player_info += "<th>bet</th>\n";
-	player_info += "<th>balance</th>\n";
-	player_info += "<th>name</th>\n";
-	player_info += "</tr>\n";
-
+	player_info += "  <thead>\n";
+	player_info += "    <tr>\n";
+	player_info += "      <th>#</th>\n";
+	player_info += "      <th>SFABDP</th>\n";  // seated, friend, active, button, dealt, playing
+	player_info += "      <th>hand</th>\n";
+	player_info += "      <th>bet</th>\n";
+	player_info += "      <th>balance</th>\n";
+	player_info += "      <th>name</th>\n";
+	player_info += "    </tr>\n";
+	player_info += "  </thead>\n";
+	// Table footer for explanations
+	player_info += "  <tfoot>\n";
+	player_info += "    <tr\n";
+	player_info += "      <td colspan=6>SFABDP = seated / friend / active / button / dealt / playing</td>\n";
+	player_info += "    </tr>\n";
+	player_info += "  </tfoot>\n";
+	
+	// Table body
+	player_info += "  <tbody>\n";
 	// One table row per player...
 	for (int i=0; i<p_tablemap->nchairs(); i++)
 	{
-		player_info += "<tr>\n";
+		player_info += "    <tr>\n";
 		// Chair number
-		player_info += "<td>%d</td>", i;   
+		text.Format("      <td>%d</td>\n", i);   
+		player_info += text;
 		// seated, friend, active, button, dealt, playing
-		text.Format("%s%s%s%s%s%s",
+		text.Format("      <td>%s%s%s%s%s%s</td>\n",
 			(int) (p_symbols->sym()->playersseatedbits) & (1<<i) ? "s" : "-",
 			p_symbols->sym()->userchair == i ? "f" : "-",
 			(int) (p_symbols->sym()->playersactivebits) & (1<<i) ? "a" : "-",
 			p_symbols->sym()->dealerchair== i ? "b" : "-",
 			(int) (p_symbols->sym()->playersdealtbits) & (1<<i) ? "d" : "-",
 			(int) (p_symbols->sym()->playersplayingbits) & (1<<i) ? "p" : "-");
-		player_info += "<td>%s</td>", text.GetString();  
+		player_info += text;  
 		// Cards
-		player_info += "<td>%s%s</td>",
+		text.Format("      <td>%s%s</td>\n",
 			GetCardHtml(p_scraper->card_player(i, 0)),
-			GetCardHtml(p_scraper->card_player(i, 1));  
+			GetCardHtml(p_scraper->card_player(i, 1)));
+		player_info += text;  
 		// Bet
-		player_info += "<td>%11.2f</td>", p_scraper->player_bet(i); 
+		text.Format("      <td>%11.2f</td>\n", p_scraper->player_bet(i));
+		player_info += text; 
 		// Balance
-		player_info += "<td>%11.2f</td>", p_scraper->player_balance(i);  
+		text.Format("      <td>%11.2f</td>\n", p_scraper->player_balance(i));
+		player_info += text;  
 		// Name
-		player_info += "<td>%-15s</td>\n", p_scraper->player_name(i).GetString();  
-		player_info += "</tr>\n";
+		text.Format("      <td>%-15s</td>\n", p_scraper->player_name(i).GetString());
+		player_info += text;
+		player_info += "    </tr>\n";
 	}
-	// Table footer for explanations
-	player_info += "<tfoot>\n";
-	player_info += "<tr colspan=6>\n";
-	player_info += "SFABDP = seated / friend / active / button / dealt / playing\n";
-	player_info += "</tr>\n";
-	player_info += "</tfoot>\n";
+	// End of table body
+	player_info += "  </tbody>\n";
 	// End of table
 	player_info += "</table>\n";
-	player_info += "</td>\n";
 	return player_info;
 }
 
@@ -252,10 +268,9 @@ CString CReplayFrame::GetButtonStatesAsHTML()
 	CString button_states, fckra_seen;
 
 	// Table header for: FCKRA
-	button_states += "<td>\n";
 	button_states += "<table align=center border=4 cellpadding=1 cellspacing=1>\n";
-	button_states += "<tr><th>FCRA</th></tr>\n";
-	button_states += "<tr>\n";
+	button_states += "  <tr><th>FCKRA</th></tr>\n";
+	button_states += "  <tr>\n";
 	// Buttons
 	int sym_myturnbits = (int) p_symbols->sym()->myturnbits;
 	fckra_seen.Format("%s%s%s%s%s",
@@ -267,39 +282,46 @@ CString CReplayFrame::GetButtonStatesAsHTML()
 		sym_myturnbits&0x04 ? "R" : ".",
 		sym_myturnbits&0x08 ? "A" : ".");
 	// Table footer
-	button_states += "<td>%s</td>\n", fckra_seen.GetString();
-	button_states += "</tr>\n";
+	button_states += "    <td>";
+	button_states += fckra_seen.GetString();
+	button_states += "</td>\n",
+	button_states += "  </tr>\n";
 	button_states += "</table>\n";
 	return button_states;
 }
 
 CString CReplayFrame::GetBlindInfoAsHTML()
 {
-	CString blind_info;
+	CString blind_info, text;
 	// Table for: sb, bb, BB
 	blind_info += "<table align=center border=4 cellpadding=1 cellspacing=1>\n";
-	blind_info += "<tr><th>sb</th><td>%11.2f</td></tr>\n", p_tablelimits->sblind();
-	blind_info += "<tr><th>bb</th><td>%11.2f</td></tr>\n", p_tablelimits->bblind();
-	blind_info += "<tr><th>BB</th><td>%11.2f</td></tr>\n", p_tablelimits->bbet();
-	blind_info += "</table>\n";
+	text.Format("  <tr><th>sb</th><td>%11.2f</td></tr>\n", p_tablelimits->sblind());
+	blind_info += text;
+	text.Format("  <tr><th>bb</th><td>%11.2f</td></tr>\n", p_tablelimits->bblind());
+	blind_info += text;
+	text.Format("  <tr><th>BB</th><td>%11.2f</td></tr>\n", p_tablelimits->bbet());
+	blind_info += text;
+	text.Format("</table>\n");
+	blind_info += text;
 	return blind_info;
 }
 
 CString CReplayFrame::GetCommonCardsAsHTML()
 {
-	CString common_cards;
+	CString common_cards, text;
 
 	// Table header for: common cards
 	common_cards += "<table align=center border=4 cellpadding=1 cellspacing=1>\n";
 	common_cards += "<tr><th>commoncard</th></tr>\n";
 	common_cards += "<tr>\n";
 	// Common cards
-	common_cards += "<td>%s%s%s%s%s</td>\n",
+	text.Format("<td>%s%s%s%s%s</td>\n",
 		GetCardHtml(p_scraper->card_common(0)),
 		GetCardHtml(p_scraper->card_common(1)),
 		GetCardHtml(p_scraper->card_common(2)),
 		GetCardHtml(p_scraper->card_common(3)),
-		GetCardHtml(p_scraper->card_common(4));
+		GetCardHtml(p_scraper->card_common(4)));
+	common_cards += text;
 	// Table footer
 	common_cards += "</tr>\n";
 	common_cards += "</table>\n";
@@ -309,23 +331,25 @@ CString CReplayFrame::GetCommonCardsAsHTML()
 
 CString CReplayFrame::GetPotsAsHTML()
 {
-	CString pots;
+	CString pots, text;
 
 	// Table header for: pots
 	pots += "<table align=center border=4 cellpadding=1 cellspacing=1>\n";
-	pots += "<tr><th>#</th><th>pot</th></tr>\n";
-	pots += "<tr>\n";
+	pots += "  <tr><th>#</th><th>pot</th></tr>\n";
+	pots += "  <tr>\n";
 	// Main pot
-	pots += "<td>0</td><td>%11.2f</td>\n", p_scraper->pot(0);
-	pots += "</tr>\n";
+	text.Format("    <td>0</td><td>%11.2f</td>\n", p_scraper->pot(0));
+	pots += text;
+	pots += "  </tr>\n";
 	// Side pots
 	for (int i=1; i<k_max_number_of_pots; i++)
 	{
 		if (p_scraper->pot(i))
 		{
-			pots += "<tr>\n";
-			pots += "<td>%d</td><td>%11.2f</td>\n", i, p_scraper->pot(i);
-			pots += "</tr>\n";
+			pots += "  <tr>\n";
+			text.Format("    <td>%d</td><td>%11.2f</td>\n", i, p_scraper->pot(i));
+			pots += text;
+			pots += "  </tr>\n";
 		}
 		else
 		{
@@ -352,10 +376,12 @@ void CReplayFrame::CreateBitMapFile()
 
 CString CReplayFrame::GetLinksToPrevAndNextFile()
 {
-	CString links;
-	links += "<a href=\"frame%06d.htm\">PREV</a>\n",
-		_next_replay_frame-1 >= 0 ? _next_replay_frame-1 : prefs.replay_max_frames();
-	links += "<a href=\"frame%06d.htm\">NEXT</a>\n",
-		_next_replay_frame+1 < prefs.replay_max_frames() ? _next_replay_frame+1 : 0;
+	CString links, text;
+	text.Format("<a href=\"frame%06d.htm\">PREV</a>\n",
+		_next_replay_frame-1 >= 0 ? _next_replay_frame-1 : prefs.replay_max_frames());
+	links += text;
+	text.Format("<a href=\"frame%06d.htm\">NEXT</a>\n",
+		_next_replay_frame+1 < prefs.replay_max_frames() ? _next_replay_frame+1 : 0);
+	links += text;
 	return links;
 }
