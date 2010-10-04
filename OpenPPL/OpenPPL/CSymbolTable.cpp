3f4643fff4837f873962e6862a585bfbfda3db66
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2010-10-04 21:11:37 +0000

    T: OpenPPL very first version

---
+++ OpenPPL/OpenPPL/CSymbolTable.cpp
@@ -0,0 +1,21 @@
+#include <afxwin.h>
+/*#include "CSymbolTable.h"
+
+CSymbolTable *p_symbol_table = 0; //NULL;
+
+CSymbolTable::CSymbolTable()
+{}
+
+CSymbolTable::~CSymbolTable()
+{}
+
+void CSymbolTable::ClearSymbolTable()
+{}
+
+void CSymbolTable::AddSymbol()
+{}
+
+bool CSymbolTable::IsOpenPPLSymbol()
+{
+	return true;
+}*/
