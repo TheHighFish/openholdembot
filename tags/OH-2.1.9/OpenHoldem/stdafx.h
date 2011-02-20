5bf5e255f2722aab6db45c4677bfb1a161caf44a
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2011-02-20 06:48:39 +0000

    Tagging OH 2.1.9 for release

---
+++ tags/OH-2.1.9/OpenHoldem/stdafx.h
@@ -0,0 +1,78 @@
+#ifndef INC_STDAFX_H
+#define INC_STDAFX_H
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently,
+// but are changed infrequently
+
+#ifndef _SECURE_ATL
+#define _SECURE_ATL 1
+#endif
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER				// Allow use of features specific to Windows XP or later.
+#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
+#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
+#endif
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
+
+// turns off MFC's hiding of some common and often safely ignored warning messages
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+// ======================================================================================
+
+#include <afxtempl.h>
+#include <afxpriv.h>
+#include <afxdlgs.h>
+
+#include <afxadv.h>			// MFC support for MRU
+
+#include <afxmt.h>			// CMutex
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Version
+#define VERSION_NUMBER				2.19
+#define VERSION_TEXT				"2.1.9"  // change version number in OpenHoldem.rc also, when needed
+
+// PokerEval
+#include "poker_defs.h"
+#include "pokereval_export.h"
+#include "evx_defs.h"
+#include "inlines/evx_inlines.h"
+
+
+#include "debug.h"
+#include "..\StructsDefines\structs_defines.h"
+
+
+#endif //INC_STDAFX_H
