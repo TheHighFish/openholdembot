d0c8e262b1c92b3266a66fd7692440e08e086e0c
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2011-12-23 05:34:20 +0000

    T: change version to 2.1.9.2 (OH)

--- branches/2.1.x_maintenance/OpenHoldem/stdafx.h
+++ branches/2.1.x_maintenance/OpenHoldem/stdafx.h
@@ -61,8 +61,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER				2.191
-#define VERSION_TEXT				"2.1.9.1"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER				2.192
+#define VERSION_TEXT				"2.1.9.2"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
