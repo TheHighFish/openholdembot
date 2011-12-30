e98070ddbd749a8da059e88c52fb067b769889bb
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2011-12-30 16:16:37 +0000

    T: OPPL: bracketify.pl

--- OpenPPL/Bracketify/bracketify.pl
+++ OpenPPL/Bracketify/bracketify.pl
@@ -55,26 +55,57 @@ while (<>)
 	# * add brackets to the left of any action
 	# * add brackets to the left of any user-defined variable to be set
 	# * add brackets to the end of a line, if there is no action
-	s/ betmax /\) BETMAX /i;
-	s/ raisemax /\) RAISEMAX /i;
-	s/ allin /\) ALLIN /i;
-	s/ betpot /\) BETPOT /i;
-	s/ raisepot /\) RAISEPOT /i;
-	s/ bethalfpot /\) BETHALFPOT /i;
-	s/ raisehalfpot /\) RAISEHALFPOT /i;
-	s/ bet /\) BET /i;
-	s/ raise /\) RAISE /i;
-	s/ call /\) CALL /i;
-	s/ check /\) CHECK /i;
-	s/ fold /\) FOLD /i;
-	s/ sitout /\) SITOUT /i;
-	# Second case: user defined variable to be set
+	# Fist case: brackets before an action
+	# We have to use more than the action keyword here,
+	# otherwise code like "BotsLastAction = Raise) would be bracketed, too.	
+	s/ betmax force/\) BETMAX FORCE/i;
+	s/ raisemax force/\) RAISEMAX FORCE/i;
+	s/ allin force/\) ALLIN FORCE/i;
+	s/ betpot force/\) BETPOT FORCE/i;
+	s/ raisepot force/\) RAISEPOT FORCE/i;
+	s/ bethalfpot force/\) BETHALFPOT FORCE/i;
+	s/ raisehalfpot force/\) RAISEHALFPOT FORCE/i;
+	s/ bet force/\) BET FORCE/i;
+	s/ raise force/\) RAISE FORCE/i;
+	s/ call force/\) CALL FORCE/i;
+	s/ check force/\) CHECK FORCE/i;
+	s/ fold force/\) FOLD FORCE/i;
+	s/ sitout force/\) SITOUT FORCE/i;
+	# Still first case: brackets before an action
+	# Now for Bet/Raise X% FORCE or Bet/Raise X Force,
+	# where Bet/Raise will be followed by a number.
+	# For OpenPPL arbitrary expressions are possible here, but..
+	# * arbitrary expressions  would be too complicated for stupid regular expressions
+	# * this tool is meant for Standard-PPL to be translated, where only numbers are possible
+	# so we check for constant numbers only
+	s/ bet 0/\) BET 0/i;	
+	s/ bet 1/\) BET 1/i;
+	s/ bet 2/\) BET 2/i;
+	s/ bet 3/\) BET 3/i;
+	s/ bet 4/\) BET 4/i;
+	s/ bet 5/\) BET 5/i;
+	s/ bet 6/\) BET 6/i;
+	s/ bet 7/\) BET 7/i;
+	s/ bet 8/\) BET 8/i;
+	s/ bet 9/\) BET 9/i;
+	s/ raise 0/\) RAISE 0/i;
+	s/ raise 1/\) RAISE 1/i;
+	s/ raise 2/\) RAISE 2/i;
+	s/ raise 3/\) RAISE 3/i;
+	s/ raise 4/\) RAISE 4/i;
+	s/ raise 5/\) RAISE 5/i;
+	s/ raise 6/\) RAISE 6/i;
+	s/ raise 7/\) RAISE 7/i;
+	s/ raise 8/\) RAISE 8/i;
+	s/ raise 9/\) RAISE 9/i;
+	# Second case: user defined variable to be set,
+	# where the line ends with a user-variable.
 
 	# Third case case: there is no action at the end,
 	# i.e. a keyword "WHEN", but no "FORCE".
 	# Then we add a bracket to the right end of the line
 	# and hope, that it is no multi-line condition
-	if ((m/^WHEN / || m/ WHEN /) && !(m/ FORCE / || m/ FORCE$/))
+	if ((m/^WHEN /i || m/ WHEN /i) && !(m/ FORCE /i || m/ FORCE$/i))
 	{
 		s/$/\)/i;	
 	}
