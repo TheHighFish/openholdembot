705b01ca8afbd683d1b62b9f122b7f6f1be8a0ba
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-02-05 11:00:56 +0000

    T: CAutoConnectorThread: resolving conflicts

--- OpenHoldem/CAutoConnectorThread.cpp
+++ OpenHoldem/CAutoConnectorThread.cpp
@@ -18,25 +18,25 @@ CAutoConnectorThread::~CAutoConnectorThread()
 // in the main application is initialized, but not earlier!
 void CAutoConnectorThread::StartThread()
 {
-	write_log(3, "CAutoConnectorThread::StartThread()\n");
+	write_log(prefs.debug_autoconnector(), "CAutoConnectorThread::StartThread()\n");
 	AfxBeginThread(AutoConnectorThreadFunction, this);
 }
 
 UINT CAutoConnectorThread::AutoConnectorThreadFunction(LPVOID pParam)
 {
-	write_log(3, "CAutoConnectorThread::AutoConnectorThreadFunction(..)\n");
+	write_log(prefs.debug_autoconnector(), "CAutoConnectorThread::AutoConnectorThreadFunction(..)\n");
 	while (true)
 	{
 		if ((prefs.autoconnector_when_to_connect() == k_AutoConnector_Connect_Permanent) && !p_autoconnector->IsConnected())
 		{
 			if(p_autoconnector->TimeSinceLastFailedAttemptToConnect() > 1 /* seconds */)
 			{
-				write_log(3, "CAutoConnectorThread: going to call Connect()");
+				write_log(prefs.debug_autoconnector(), "CAutoConnectorThread: going to call Connect()");
 				p_autoconnector->Connect(NULL);
 			}
 			else
 			{
-				write_log(3, "CAutoConnectorThread: Reconnection blocked. Other instance failed previously.\n");
+				write_log(prefs.debug_autoconnector(), "CAutoConnectorThread: Reconnection blocked. Other instance failed previously.\n");
 			}
 		}
 		Sleep(1000); // 1000 milli-seconds
