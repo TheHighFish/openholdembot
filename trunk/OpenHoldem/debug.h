18b8cb7b4972fb8204ebc8c96473013ca1f849fa
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-02-05 12:19:29 +0000

    T: Merging debug.h

--- trunk/OpenHoldem/debug.h
+++ trunk/OpenHoldem/debug.h
@@ -14,11 +14,11 @@ char * get_now_time(char * timebuf);
 BOOL CreateBMPFile(const char *szFile, HBITMAP hBMP);
 
 void start_log(void);
-void write_log(int level, char* fmt, ...);
-void write_log_nostamp(int level, char* fmt, ...);
-void write_logautoplay(int level, const char * action);
+void write_log(bool debug_settings_for_this_message, char* fmt, ...);
+void write_log_nostamp(bool debug_settings_for_this_message, char* fmt, ...);
+void write_logautoplay(const char * action);
 void stop_log(void);
-void write_log_pokertracker(int level, char* fmt, ...);
+void write_log_pokertracker(bool debug_settings_for_this_message, char* fmt, ...);
 int GenerateDump(EXCEPTION_POINTERS *pExceptionPointers);
 
 extern FILE *log_fp;
