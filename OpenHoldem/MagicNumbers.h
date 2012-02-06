162b4c9fddac890445d8bccbdc3c84f84b9e4470
strikles <strikles@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-02-06 01:29:27 +0000

    T: Resolving easy CSymbols conflicts

--- OpenHoldem/MagicNumbers.h
+++ OpenHoldem/MagicNumbers.h
@@ -36,6 +36,7 @@ const int k_max_chair_number = k_max_number_of_players - 1;
 const int k_number_of_community_cards  = 5;
 const int k_number_of_flop_cards       = 3;  
 const int k_number_of_cards_per_player = 2;
+const int k_number_of_cards_per_deck = 52;
 
 // Game type
 const int k_gametype_unknown = -1;
