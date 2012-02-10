cd47e4c5db68aefb3da39b65b2860b154093eb9f
strikles <strikles@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-02-10 19:17:23 +0000

    T: (CStableFramesCounter.cpp) Re-factoring for f$delay fix

--- trunk/OpenHoldem/CStableFramesCounter.h
+++ trunk/OpenHoldem/CStableFramesCounter.h
@@ -7,13 +7,23 @@ public:
 	CStableFramesCounter();
 	~CStableFramesCounter();
 public:
-	void ResetOnAutoplayerAction();
-	int  NumberOfStableFrames() {return _NumberOfStableFrames;}
-	int  UpdateNumberOfStableFrames();
+	void			ResetOnAutoplayerAction();
+	unsigned int	NumberOfStableFrames() {return _NumberOfStableFrames;}
+	unsigned int	UpdateNumberOfStableFrames();
 private:
-	void Reset();
+	void			Reset();
+	void			SaveCurrentState();
 private:
-	int _NumberOfStableFrames;
+	unsigned int	_NumberOfStableFrames;
+	bool			_isReset;
+private:
+	// variables to keep the last game-state.
+	unsigned int	_myturnbitslast;
+	unsigned int	_card_common_last[k_number_of_community_cards];
+	unsigned int	_card_player_last[k_max_number_of_players][k_number_of_cards_per_player];
+	bool			_dealer_last[k_max_number_of_players];
+	double			_playerbalance_last[k_max_number_of_players];
+	double			_playerbet_last[k_max_number_of_players];
 } *p_stableframescounter;
 
 #endif // _INC_CSTABLEFRAMESCOUNTER_H
