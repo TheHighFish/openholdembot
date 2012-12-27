5db9364b25026c3fc4f0d793535978e4327ce66d
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-12-27 10:01:16 +0000

    T: OH: creating a feature-branch to scrape overlapping tables

---
+++ OpenHoldem_scraping_overlapping_tables_development/CSymbolEngineRandom.h
@@ -0,0 +1,35 @@
+#ifndef INC_CSYMBOLENGINERANDOM_H
+#define INC_CSYMBOLENGINERANDOM_H
+
+#include "CVirtualSymbolEngine.h"
+#include "..\CTransform\CTransform.h"
+
+extern class CSymbolEngineRandom: public CVirtualSymbolEngine
+{
+public:
+	CSymbolEngineRandom();
+	~CSymbolEngineRandom();
+public:
+	// Mandatory reset-functions
+	void InitOnStartup();
+	void ResetOnConnection();
+	void ResetOnHandreset();
+	void ResetOnNewRound();
+	void ResetOnMyTurn();
+	void ResetOnHeartbeat();
+public:
+	// Public accessors
+	// "random" gets calculated each time new
+	double random()				{ return Random_0_1(); }
+	double randomhand()			{ return _randomhand; }
+	double randomheartbeat()	{ return _randomheartbeat; }
+	double randomround()		{ return _randomround; }
+private:
+	double Random_0_1();
+private:
+	double _randomhand;
+	double _randomheartbeat;
+	double _randomround;
+} *p_symbol_engine_random;
+
+#endif INC_CSYMBOLENGINERANDOM_H
