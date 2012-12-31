35c086d09816c41a22439fa754f31917ff244cf0
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2012-12-31 06:00:18 +0000

    T: OH: some comments

--- OpenHoldem/CScraper.cpp
+++ OpenHoldem/CScraper.cpp
@@ -1,3 +1,20 @@
+//*****************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//*****************************************************************************
+//
+// Purpose: Scraping the poker-table and providing access to the scraped data.
+//  As the CScraper is low-level and quite large we created 
+//  a interface SCraperAccess that provides higher-level accessors
+//  like "UserHasCards()".
+//  Better use that interface to access scraper-data whenever possible.
+//
+//*****************************************************************************
+
 #include "StdAfx.h"
 #include "CScraper.h"
 
