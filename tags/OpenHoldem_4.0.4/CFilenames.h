23beb6606c2d50fa74bb0483b0d83487929cd19d
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-03-12 10:06:41 +0000

    Tagging OpenHoldem 4.0.4 for release

---
+++ tags/OpenHoldem_4.0.4/CFilenames.h
@@ -0,0 +1,25 @@
+extern class CFilenames
+{
+public:
+	CFilenames();
+	~CFilenames();
+public:
+	CString OpenHoldemDirectory();
+	CString IniFilePath();
+	CString TableMapWildcard();
+	CString LogFilename();
+	CString DebugTabLogFilename();
+	CString PokerTrackerLogFilename();
+	CString MiniDumpFilename();
+	CString ScraperDirectory();
+	CString ReplaySessionDirectory();
+	CString ReplayBitmapFilename(int frame_number);
+	CString ReplayHTMLFilename(int frame_number);
+	CString PathOfExecutable();
+public:
+	void SwitchToOpenHoldemDirectory();
+private:
+	CString IniFilename();
+private:
+	char _startup_path[MAX_PATH];
+} *p_filenames;
