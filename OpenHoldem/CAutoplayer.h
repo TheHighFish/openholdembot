4b1322851642b262cebf4ae99ee065aed59b8b18
OHZooBoy <OHZooBoy@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-04-25 20:18:56 +0000

    T: Remove unused functions/declarations for CAutoplayer

--- OpenHoldem/CAutoplayer.h
+++ OpenHoldem/CAutoplayer.h
@@ -30,7 +30,6 @@ private:
 	bool ExecutePrimaryFormulasIfNecessary();
 	bool ExecuteSecondaryFormulasIfNecessary();
 	bool ExecuteRaiseCallCheckFold();
-	bool AnyPrimaryFormulaTrue();
 	bool AnySecondaryFormulaTrue();
 	bool DoAllin(void);
 	bool DoBetPot(void);
