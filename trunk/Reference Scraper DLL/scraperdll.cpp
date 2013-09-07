4755f36593d3d415f89d3c97fd611487cd16958a
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-09-07 09:23:19 +0000

    T: OH: adding a copy-right-notice to some files

--- trunk/Reference Scraper DLL/scraperdll.cpp
+++ trunk/Reference Scraper DLL/scraperdll.cpp
@@ -1,5 +1,15 @@
-// scraperdll.cpp : Defines the entry point for the DLL application.
+//*****************************************************************************
 //
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//*****************************************************************************
+//
+// Purpose: craperdll.cpp : Defines the entry point for the DLL application.
+//
+//*****************************************************************************
 
 #include <windows.h>
 #include "scraperdll.h"
