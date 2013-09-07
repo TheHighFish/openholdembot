1fb6d767bcf5fa971a79abdae0e8e7f02b9631aa
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-09-07 10:03:13 +0000

    T: OS: adding a copy-right-notice to some files

--- trunk/OpenScrape/OpenScrapeDoc.cpp
+++ trunk/OpenScrape/OpenScrapeDoc.cpp
@@ -1,3 +1,17 @@
+//*****************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//*****************************************************************************
+//
+// Purpose:
+//
+//*****************************************************************************
+
+
 // OpenScrapeDoc.cpp : implementation of the COpenScrapeDoc class
 //
 
