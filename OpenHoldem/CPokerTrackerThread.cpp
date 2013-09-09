f97d66c4bc63a3436f6164118558cc56ab6eaa7c
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-09-09 09:39:34 +0000

    T: OH: improved PT-code (siteID and logging)

--- OpenHoldem/CPokerTrackerThread.cpp
+++ OpenHoldem/CPokerTrackerThread.cpp
@@ -65,45 +65,37 @@ CPokerTrackerLookup::~CPokerTrackerLookup()
 
 const int CPokerTrackerLookup::GetSiteId()
 {
-	CString network = p_tablemap->network();
-
+	// Is s$sitename or s$network one of the supported PT sites? 
+	// Return the proper site_id for db queries.
+	// PT version 3 only
+	//
 	// No longer requiring an exact match for manualmode,
-	// but treating it like a normal casino.
-	// http://www.maxinmontreal.com/forums/viewtopic.php?f=114&t=12158&p=108712#p108712
+    // but treating it like a normal casino.
+    // http://www.maxinmontreal.com/forums/viewtopic.php?f=114&t=12158&p=108712#p108712
+
 	std::map<CString, int>::const_iterator lookup, end;
-
-	// Is s$sitename one of the supported PT sites?  Return the proper site_id for db queries.
-	// PT version 3 only
 	lookup = _pt3_siteid.begin();
 	end = _pt3_siteid.end();
 
 	while (lookup!=end)
 	{
-		CString	sym = "sitename$" + lookup->first;
-		int e = SUCCESS;
-
-		if (p_symbols->GetSymbolVal(sym.MakeLower().GetString(), &e))
+		CString supported_sitename = lookup->first;
+		if (p_tablemap->sitename().Find(supported_sitename)
+			|| p_tablemap->network().Find(supported_sitename))
+		{
+			write_log(preferences.debug_pokertracker(), "[PokerTrackerLookup] Site or network is: %s\n",
+				supported_sitename);
+			write_log(preferences.debug_pokertracker(), "[PokerTrackerLookup] SiteID is %i\n",
+				lookup->second);
 			return lookup->second;
-
+		}
 		lookup++;
 	}
-
-	// Is s$network one of the supported PT sites?  Return the proper site_id for db queries.
-	// PT version 3 only
-	lookup = _pt3_siteid.begin();
-	end = _pt3_siteid.end();
-
-	while (lookup!=end)
-	{
-		CString	sym = "network$" + lookup->first;
-		int e = SUCCESS;
-
-		if (p_symbols->GetSymbolVal(sym.MakeLower().GetString(), &e))
-			return lookup->second;
-
-		lookup++;
-	}
-	return -1 ;
+	write_log(preferences.debug_pokertracker(), 
+		"[PokerTrackerLookup] ERROR: unsupported sitename and unsupported network.\n");
+	write_log(preferences.debug_pokertracker(), 
+		"[PokerTrackerLookup] Probably wrong data in tablemap.\n");
+	return -1;
 }
 
 CString CPokerTrackerThread::CreateConnectionString(
