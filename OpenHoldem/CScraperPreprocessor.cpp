5c0ea781c75678de26b444306e88a9d9db88f87d
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-09-09 20:45:42 +0000

    T: OH: new internal CScraperPreprocessor.cpp

---
+++ OpenHoldem/CScraperPreprocessor.cpp
@@ -0,0 +1,27 @@
+//*****************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//*****************************************************************************
+//
+// Purpose:
+//
+//*****************************************************************************
+
+#include "stdafx.h"
+#include "CScraperPreprocessor.h"
+
+CScraperPreprocessor::CScraperPreprocessor()
+{}
+
+CScraperPreprocessor::~CScraperPreprocessor()
+{}
+
+void CScraperPreprocessor::PreprocessMonetaryString(CString *monetary_string)
+{}
+
+void CScraperPreprocessor::PreprocessTitleString(CString *title_string)
+{}
