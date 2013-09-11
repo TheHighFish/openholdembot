7bf15b159023a89d4e13f7b1f02b59d41a073493
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-09-11 16:38:32 +0000

    T: OH: change version to 4.2.3

--- OpenHoldem/stdafx.h
+++ OpenHoldem/stdafx.h
@@ -88,8 +88,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER				4.22
-#define VERSION_TEXT				"4.2.2"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER				4.23
+#define VERSION_TEXT				"4.2.3"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
