adc1ca7736a4601448e1869085363c41cd64c4eb
pokers.czesio <pokers.czesio@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2013-12-09 21:07:53 +0000

    Tagging OpenHoldem 4.3.1 for release

---
+++ tags/OpenHoldem_4.3.1/OpenHoldem/StringFunctions.cpp
@@ -0,0 +1,52 @@
+//***************************************************************************** 
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//***************************************************************************** 
+//
+// Purpose: Providing functions that deal with strings
+//
+//***************************************************************************** 
+
+#include <stdafx.h>
+#include "NumericalFunctions.h"
+#include "StringFunctions.h"
+
+bool StringIsExactMatch(const char *string_a, const char *string_b)
+{
+	return ((strlen(string_a) == strlen(string_b))
+		&& StringAIsPrefixOfStringB(string_a, string_b));
+}
+
+bool StringAIsPrefixOfStringB(const char *string_a, const char *string_b)
+{
+	if (strlen(string_a) > strlen(string_b))
+	{
+		return false;
+	}
+	// Result of memcmp == 0 means: identical
+	// See http://www.cplusplus.com/reference/clibrary/cstring/memcmp/
+	return (memcmp(string_a, string_b, strlen(string_a)) == 0);
+}
+
+CString Bool2CString(bool b)
+{
+	return (b ? "true" : "false");
+}
+
+CString Number2CString(double number)
+{
+	CString result;
+	if (IsInteger(number))
+	{
+		result.Format("%1.3f", number);
+	}
+	else
+	{
+		result.Format("%1.0d", number);
+	}
+	return result;
+}
