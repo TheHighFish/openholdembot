95119e58364ea4a2c90037ab96873c1f7db76726
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-02-05 23:02:58 +0000

    T: OH-scraper-optimization: several small improvements

--- OpenHoldem_Scraper_Optimization/CDllExtension.h
+++ OpenHoldem_Scraper_Optimization/CDllExtension.h
@@ -20,8 +20,8 @@ typedef double (*process_message_t)(const char* message, const void* param);
 struct SHoldemPlayer 
 {
 	char			m_name[16]		  ;	//player name if known
-	double			m_balance		   ;	//player balance 
-	double			m_currentbet		;	//player current bet
+	double			m_balance		  ;	//player balance 
+	double			m_currentbet	  ;	//player current bet
 	unsigned char   m_cards[2]		  ;	//player cards
 
 	unsigned char   m_name_known	: 1 ;	//0=no 1=yes
@@ -33,15 +33,15 @@ struct SHoldemPlayer
 struct SHoldemState 
 {
 	char			m_title[64]		 ;	//table title
-	double			m_pot[10]		   ;	//total in each pot
+	double			m_pot[10]		 ;	//total in each pot
 
-	unsigned char   m_cards[5]		  ;	//common cards
+	unsigned char   m_cards[5]		 ;	//common cards
 
 	unsigned char   m_is_playing	: 1 ;	//0=sitting-out, 1=sitting-in
 	unsigned char   m_is_posting	: 1 ;	//0=autopost-off, 1=autopost-on
 	unsigned char   m_fillerbits	: 6 ;	//filler bits
 
-	unsigned char   m_fillerbyte		;	//filler byte
+	unsigned char   m_fillerbyte	  ;	//filler byte
 	unsigned char   m_dealer_chair	  ;	//0-9
 
 	SHoldemPlayer   m_player[10]		;	//player records
