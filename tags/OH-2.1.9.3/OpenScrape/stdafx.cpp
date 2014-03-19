afd710aa1d8f4fe18bc3b8f928061d71122a4cc1
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-03-19 20:35:16 +0000

    Various code-cleanups

--- tags/OH-2.1.9.3/OpenScrape/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// OpenScrape.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
