decfdcd146682935efe953161b42b0c52afbe2f7
thehighfish <thehighfish@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-05-04 21:49:19 +0000

    T: OH-new-parser: hand-lists

--- trunk/OpenHoldem_NewParser/COHScriptList.h
+++ trunk/OpenHoldem_NewParser/COHScriptList.h
@@ -16,16 +16,40 @@
 
 #include "COHScriptObject.h"
 
-class COHScriptList: public COHScriptObject
-{
-public:
-	COHScriptList(CString *new_name, CString *new_function_text);
-	~COHScriptList();
-public:
-	void Set(CString list_member);
-	// Actually Evaluate() returns true/false if our hand is in the list or not
-	// but the function is inheritzed from the base-class.
-	double Evaluate();
+typedef bool THandListMatrix[k_number_of_ranks_per_deck][k_number_of_ranks_per_deck];
+//   2 3 4 5 6 7 8 9 T J Q K A
+// 2 P
+// 3   A
+// 4     I     SUITED  
+// 5       R
+// 6         S
+// 7           P
+// 8             A
+// 9               I
+// T                 R
+// J                   S 
+// Q    OFFSUITED        P
+// K                       A   
+// A                         I
+
+class COHScriptList: public COHScriptObject {
+ public:
+  COHScriptList(CString *new_name, CString *new_function_text);
+  ~COHScriptList();
+ public:
+  void Clear();
+  void Set(CString list_member);
+  // Actually Evaluate() returns true/false if our hand is in the list or not
+  // but the function is inheritzed from the base-class.
+  double Evaluate();
+ private:
+  void ErrorInvalidMember(CString list_member); 
+  void ErrorOldStyleFormat(CString list_member);
+ private:
+  void Set(int first_rank, int second_rank, bool suited);
+  bool IsOnList(int first_rank, int second_rank, bool suited);
+ private:
+  THandListMatrix _handlist_matrix;
 };
 
 #endif INC_COHSCRIPTLIST_H
