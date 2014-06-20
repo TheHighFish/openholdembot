812c18faa7f4cc8d61012a9d1902308e22a78399
thehighfish@gmail.com <thehighfish@gmail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-06-20 18:29:39 +0000

    T: Development: deleting old unused branches

--- branches/OpenHoldem_Autoplayer_Synchronization/OpenPPL/OpenPPL/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_MAIN_H
-#define INC_MAIN_H
-
-#include <sstream> 
-
-// A global variable, used by multiple modules. ;-)
-extern std::stringstream current_output;
-
-#endif
--- branches/Openholdem_light_weight/OpenPPL/OpenPPL/LineCounter.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_LINECOUNTER_H
-
-#define INC_LINECOUNTER_H
-
-static void IncLineCounter();
-int GetLineCount();
-
-#endif // INC_LINECOUNTER_H
