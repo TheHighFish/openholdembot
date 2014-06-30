cb160bc8b5c114ed5de595d21ff8414f435a4bb2
thehighfish@gmail.com <thehighfish@gmail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-06-30 19:50:03 +0000

    T: OH: improved cplusplusify.pl

--- OpenPPL/OpenPPL_Library/cplusplusify.pl
+++ OpenPPL/OpenPPL_Library/cplusplusify.pl
@@ -1,15 +1,32 @@
 #   Download page:         http://code.google.com/p/openholdembot/
 #   Forums:                http://www.maxinmontreal.com/forums/index.php
 #   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
-
+#
 # Simple script that translates the OpenPPL-library to (nearly) C++
 # for the new OpenPPL symbol-engines.
+
+print "//*****************************************************************************\n";
+print "//\n";
+print "// This file is part of the OpenHoldem project\n";
+print "//   Download page:         http://code.google.com/p/openholdembot/\n";
+print "//   Forums:                http://www.maxinmontreal.com/forums/index.php\n";
+print "//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html\n";
+print "//\n";
+print "//*****************************************************************************\n";
+print "//\n";
+print "// Purpose:\n";
+print "//\n";
+print "//*****************************************************************************\n";
+print "\n";
+
 use strict;
+my $classname          = "CSymbolEngineOpenPPLHandStrength";	# Configure this
 my $outside_function   = 0;
 my $start_of_function  = 1;
 my $inside_function    = 2;
 my $parsing_state      = $outside_function;
 my $current_line;
+my @list_of_headers;
 
 while (<>) {
 	chomp;
@@ -60,11 +77,13 @@ while (<>) {
 sub function_header {		
 	$parsing_state = $start_of_function;
 	s/##//g;
-	s/f\$/double Calculate/;
-	print;
-	print "()    // Please revisit the return type. int or bool might be more appropriate\n";
-	print "{\n";
-	print "    return (";
+	s/^f\$//;
+	my $function_header = "double " . $classname . "::Calculate" . $_ . "()";
+	push(@list_of_headers, $function_header);
+	print $function_header;
+	print " {\n";
+	print "// Please revisit the return type. int or bool might be more appropriate\n";
+	print "  return (";
 }
 
 sub comment {
@@ -117,3 +136,21 @@ sub empty_space {
 	print "\n";
 	print;
 }
+
+#Finally generate class declaration
+print "\n\n\n\n";
+print "class ";
+print $classname;
+print " {\n";
+print " public:\n";
+print "  ";
+print $classname; 
+print "();\n";
+print "  ~" . $classname . "();\n";
+print " private:\n";
+foreach (@list_of_headers) {
+	print "  ";
+	print;
+	print ";\n";
+}
+print "};"
