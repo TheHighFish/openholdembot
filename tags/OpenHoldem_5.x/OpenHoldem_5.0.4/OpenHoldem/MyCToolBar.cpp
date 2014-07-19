d35934788b3af4e90b33f5fc14e49fee293b00a8
thehighfish@gmail.com <thehighfish@gmail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-07-19 08:18:51 +0000

    Tagging OpenHoldem 5.0.4 for release

---
+++ tags/OpenHoldem_5.x/OpenHoldem_5.0.4/OpenHoldem/MyCToolBar.cpp
@@ -0,0 +1,39 @@
+//*****************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//*****************************************************************************
+//
+// Purpose:
+//
+//*****************************************************************************
+
+// MainToolBar.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "OpenHoldem.h"
+#include "MyCToolBar.h"
+
+// CMyToolBar
+
+IMPLEMENT_DYNAMIC(CMyToolBar, CToolBar)
+
+CMyToolBar::CMyToolBar() 
+{
+}
+
+CMyToolBar::~CMyToolBar() 
+{
+}
+
+BEGIN_MESSAGE_MAP(CMyToolBar, CToolBar)
+END_MESSAGE_MAP()
+
+
+void CMyToolBar::OnUpdateCmdUI(CFrameWnd* pTarget, BOOL bDisableIfNoHandler) 
+{
+}
