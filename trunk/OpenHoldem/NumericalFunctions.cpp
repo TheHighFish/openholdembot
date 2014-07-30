0451924990d7ae9f2b5d65d9e409f773a2695d2b
thehighfish@gmail.com <thehighfish@gmail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-07-30 11:16:07 +0000

    T: OH: parsing bin and hex numbers

--- trunk/OpenHoldem/NumericalFunctions.cpp
+++ trunk/OpenHoldem/NumericalFunctions.cpp
@@ -15,6 +15,25 @@
 #include "stdafx.h"
 #include "NumericalFunctions.h"
 
+// Deals with floating points, ints, hex and binary
+double StringToNumber(CString number) {
+  // http://www.cplusplus.com/reference/cstdlib/strtol/
+  // http://stackoverflow.com/questions/10156409/convert-hex-string-char-to-int
+  assert(number[0] != NULL);
+  if (memcmp(number, "0x", 2) == 0) {
+    // Hex number
+    return strtol(number, NULL, 16);
+  }
+  else if (memcmp(number, "0b", 2)) {
+    // Binary number
+    return strtol(number, NULL, 16);
+  }
+  else {
+    // Decimal number, either int or floating point
+    return atof(number);
+  }
+}
+
 #define TWO(c) (0x1u << (c))
 #define MASK(c) (((unsigned int)(-1)) / (TWO(TWO(c)) + 1u))
 #define COUNT(x,c) ((x) & MASK(c)) + (((x) >> (TWO(c))) & MASK(c))
