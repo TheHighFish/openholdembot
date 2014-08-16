28f273163729a9986969b12d1e29de67cb44fdba
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-08-16 16:01:09 +0000

    T: OH7: removed unused iter_vars

--- branches/OH7_dev/OpenHoldem/CSymbolEnginePrwin.cpp
+++ branches/OH7_dev/OpenHoldem/CSymbolEnginePrwin.cpp
@@ -43,9 +43,7 @@ void CSymbolEnginePrwin::InitOnStartup()
 	ResetOnConnection();
 }
 
-void CSymbolEnginePrwin::ResetOnConnection()
-{
-	//!!!!!iter_vars.ResetVars();
+void CSymbolEnginePrwin::ResetOnConnection() {
 	ResetOnHandreset();
 }
 
