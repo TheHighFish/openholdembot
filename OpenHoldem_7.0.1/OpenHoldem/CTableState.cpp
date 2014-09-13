1cd7db8ff5238a775ae2b5daf0292b38706c8374
nahman.poker@gmail.com <nahman.poker@gmail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-09-13 13:07:48 +0000

    Tagging OpenHoldem 7.0.1 for release

---
+++ OpenHoldem_7.0.1/OpenHoldem/CTableState.cpp
@@ -0,0 +1,46 @@
+//******************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//******************************************************************************
+//
+// Purpose: Game-state object for the table
+//   (common cards and players)
+//   Actually it should be names CGameSate,
+//   but there already is a symbol engine with that name. 
+//
+//******************************************************************************
+
+#include "stdafx.h"
+#include "CSymbolEngineUserchair.h"
+#include "CTableState.h"
+#include "NumericalFunctions.h"
+
+CTableState *p_table_state = NULL;
+
+CTableState::CTableState() {
+  for (int i=0; i<k_number_of_community_cards; ++i) {
+    _common_cards[i].ClearValue();
+  }
+  for (int i=0; i<kNumberOfPlayerEntries; ++i) {
+    _players[i].Reset();
+  }
+}
+
+CTableState::~CTableState() {
+}
+
+CPlayer *CTableState::User() {
+  if (p_symbol_engine_userchair == NULL) {
+    return &_players[kFakeEntryForUnknownUserchair];
+  }
+  if (!p_symbol_engine_userchair->userchair_confirmed()) {
+    return &_players[kFakeEntryForUnknownUserchair];
+  }
+  int userchair = p_symbol_engine_userchair->userchair();
+  AssertRange(userchair, 0, k_last_chair);
+  return &_players[userchair];
+}
