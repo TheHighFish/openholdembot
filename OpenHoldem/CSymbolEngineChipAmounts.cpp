cf57ce82c9397224be411431ccb6293e8d448a9b
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-09-15 15:18:04 +0000

    T: OH: replaced assertion by "error-handling" and warning

--- OpenHoldem/CSymbolEngineChipAmounts.cpp
+++ OpenHoldem/CSymbolEngineChipAmounts.cpp
@@ -168,7 +168,11 @@ void CSymbolEngineChipAmounts::CalculatePots() {
 		}
 		_pot = _potcommon + _potplayer;
 	}
-  assert(_potcommon >= 0.0);
+  if (_potcommon < 0) {
+	// This can happen for potmethod = 2 and incorrectly scraped (occluded) main-pot
+	write_log(k_always_log_errors, "[CSymbolEngineChipAmounts] ERROR: negative potcommon. Probably miss-scraped main-pot. Adapting to 0.0\n"
+	_potcommon = 0;
+  }
 }
 
 void CSymbolEngineChipAmounts::CalculateAmountsToCallToRaise() 
