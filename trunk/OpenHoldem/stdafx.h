aece6a886a30a4a2dfc406fd15c097e6a8ea2698
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2014-11-20 16:21:02 +0000

    T: OH: change version to 7.3.0

--- trunk/OpenHoldem/stdafx.h
+++ trunk/OpenHoldem/stdafx.h
@@ -89,8 +89,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER			7.22
-#define VERSION_TEXT				"7.2.2"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER			7.30
+#define VERSION_TEXT				"7.3.0"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
