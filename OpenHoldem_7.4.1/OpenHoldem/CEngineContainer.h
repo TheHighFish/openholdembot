473a664389447816d3101d315df4de7d816ce26a
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2015-01-13 10:48:50 +0000

    Tagging OpenHoldem 7.4.1 for release

---
+++ OpenHoldem_7.4.1/OpenHoldem/CEngineContainer.h
@@ -0,0 +1,55 @@
+//******************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//******************************************************************************
+//
+// Purpose:
+//
+//******************************************************************************
+
+#ifndef INC_CENGINECONTAINER_H
+#define INC_CENGINECONTAINER_H
+
+#include "CVirtualSymbolEngine.h"
+
+const int k_max_number_of_symbol_engines = 100;
+
+class CEngineContainer {
+ public:
+  CEngineContainer();
+  ~CEngineContainer();
+ public:
+  // both to be called by the auto-connector
+  void ResetOnConnection();
+  void ResetOnDisconnection();
+ public:
+  void EvaluateAll();
+  bool EvaluateSymbol(const char *name, double *result, bool log = false);
+  CString SymbolsProvided()       { BuildListOfSymbolsProvided(); return _list_of_symbols; }
+ private:
+  void BuildListOfSymbolsProvided();
+ private:
+  void CreateSpecialSymbolEngines();
+  void AddSymbolEngine(CVirtualSymbolEngine *new_symbol_engine);
+  void CreateSymbolEngines();
+  void DestroyAllSymbolEngines();
+  void DestroyAllSpecialSymbolEngines();
+ private:
+  void ResetOnHandreset();
+  void ResetOnNewRound();
+  void ResetOnMyTurn();
+  void ResetOnHeartbeat();
+ private:
+  CVirtualSymbolEngine *_symbol_engines[k_max_number_of_symbol_engines]; 
+  int _number_of_symbol_engines_loaded;
+  bool _reset_on_connection_executed;
+  CString _list_of_symbols;
+};
+
+extern CEngineContainer *p_engine_container;
+
+#endif INC_CENGINECONTAINER_H
---
+++ OpenHoldem_7.4.1/OpenHoldem/DialogSAPrefs17.cpp
@@ -0,0 +1,69 @@
+//******************************************************************************
+//
+// This file is part of the OpenHoldem project
+//   Download page:         http://code.google.com/p/openholdembot/
+//   Forums:                http://www.maxinmontreal.com/forums/index.php
+//   Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//******************************************************************************
+//
+// Purpose:
+//
+//******************************************************************************
+
+// DialogSAPrefs1.cpp : implementation file
+//
+
+#include "stdafx.h"
+
+#include "SAPrefsSubDlg.h"
+#include "DialogSAPrefs17.h"
+#include "CPreferences.h"
+
+// CDlgSAPrefs17 dialog
+
+IMPLEMENT_DYNAMIC(CDlgSAPrefs17, CSAPrefsSubDlg)
+
+CDlgSAPrefs17::CDlgSAPrefs17(CWnd* pParent /*=NULL*/)
+		: CSAPrefsSubDlg(CDlgSAPrefs17::IDD, pParent)
+{
+}
+
+CDlgSAPrefs17::~CDlgSAPrefs17()
+{
+}
+
+void CDlgSAPrefs17::DoDataExchange(CDataExchange* pDX)
+{
+	CSAPrefsSubDlg::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_CONFIGURATION_INPUT_SETTINGS, m_ConfigurationInputSettings_Button);
+	DDX_Control(pDX, IDC_CONFIGURATION_THEME_SETTINGS, m_ConfigurationThemeSettings_Button);
+	DDX_Control(pDX, IDC_CONFIGURATION_FONT_SETTINGS, m_ConfigurationFontSettings_Button);
+}
+
+BEGIN_MESSAGE_MAP(CDlgSAPrefs17, CSAPrefsSubDlg)
+  ON_BN_CLICKED(IDC_CHECK1, &CDlgSAPrefs17::OnBnClickedCheck1)
+END_MESSAGE_MAP()
+
+// CDlgSAPrefs1 message handlers
+BOOL CDlgSAPrefs17::OnInitDialog()
+{
+	CSAPrefsSubDlg::OnInitDialog();
+	m_ConfigurationInputSettings_Button.SetCheck(preferences.configurationcheck_input_settings() != 0);
+	m_ConfigurationThemeSettings_Button.SetCheck(preferences.configurationcheck_theme_settings() != 0);
+	m_ConfigurationFontSettings_Button.SetCheck(preferences.configurationcheck_font_settings() != 0);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CDlgSAPrefs17::OnOK()
+{
+	preferences.SetValue(k_prefs_configurationcheck_input_settings, m_ConfigurationInputSettings_Button.GetCheck());
+	preferences.SetValue(k_prefs_configurationcheck_theme_settings, m_ConfigurationThemeSettings_Button.GetCheck());
+	preferences.SetValue(k_prefs_configurationcheck_font_settings, m_ConfigurationFontSettings_Button.GetCheck());
+	CSAPrefsSubDlg::OnOK();
+}
+
+void CDlgSAPrefs17::OnBnClickedCheck1()
+{}
