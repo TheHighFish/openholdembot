b45d6cfacfa631904dd8f36d23c84248a57ac6f7
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2015-01-22 20:01:42 +0000

    T: OH: preparing release

--- trunk/OpenHoldem/stdafx.h
+++ trunk/OpenHoldem/stdafx.h
@@ -91,8 +91,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER			7.41
-#define VERSION_TEXT				"7.4.1"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER			7.42
+#define VERSION_TEXT				"7.4.2"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
