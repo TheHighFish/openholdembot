5973535a81a296ec06e699ee219191f64ccf7817
thehighfish@googlemail.com <thehighfish@googlemail.com@b0b9fd3a-3a3c-0410-a9aa-dfaef611493d>
2015-04-01 10:31:45 +0000

    Tagging OpenHoldem 7.7.2 for release

---
+++ OpenHoldem_7.7.2/RegressionTests/Tools/TestSuite2/testsuite_utility.py
@@ -0,0 +1,12 @@
+
+class LogStyle():
+    NORMAL = 1
+    TITLE = 2
+    SUCCESS = 3
+    ERROR = 4
+    
+class ItemIcon():
+    DEFAULT = 1
+    ERROR = 2
+    FAILED = 3
+    SUCCESS = 4
---
+++ OpenHoldem_7.7.2/RegressionTests/setPythonEnv.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem set the location of the python 3.4 binary
+set PythonBin="C:\Python34\python.exe"
+
+@echo on
