84650d84289d0c7d1ed52a1c65f913a2ed01fe5c
TheHighFish <thehighfish@googlemqail.com>
2015-10-15 13:40:49 +0200

    Improved problem-solver

--- OpenHoldem/CProblemSolver.cpp
+++ OpenHoldem/CProblemSolver.cpp
@@ -93,8 +93,9 @@ void CProblemSolver::TryToDetectBeginnersProblems()
 			"\n"
 			" To connect to a table three conditions must be met:\n"
 			"    - You need a tablemap for that casino and game-type.\n"
-			"    - The tablesize must be right.\n"
-			"    - The titlestring must match.\n"
+			"    - The z$clientsize must be right.\n"
+      "         OpenScrape -> Menu -> View\n"
+			"    - The s$titletext must match.\n"
 			"If OpenHoldem does not connect, then you have to fix your tablemap.",
 			k_title_string, 0);
 	}
