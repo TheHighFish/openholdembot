a57da1458597689f2861d3377937a467c8807ffd
TheHighFish <thehighfish@googlemail.com>
2016-03-28 21:31:27 +0200

    Changed version to 9.1.1

--- ManualMode/stdafx.h
+++ ManualMode/stdafx.h
@@ -65,8 +65,8 @@
 
 
 // Version
-#define VERSION_NUMBER				4.41
-#define VERSION_TEXT				"4.4.1.0"  // change version number in ManualMode.rc also, when needed
+#define VERSION_NUMBER				9.11
+#define VERSION_TEXT				"9.1.1.0"  // change version number in ManualMode.rc also, when needed
 
 
 
