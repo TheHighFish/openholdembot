367ad231cf1c5169c95cc2516658750cacc45f2a
TheHighFish <thehighfish@googlemail.com>
2016-07-21 05:49:22 +0200

    Last changes for 9.2.1

--- OpenHoldem/stdafx.h
+++ OpenHoldem/stdafx.h
@@ -134,8 +134,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER			9.20
-#define VERSION_TEXT				"9.2.0.0"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER			9.21
+#define VERSION_TEXT				"9.2.1.0"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
