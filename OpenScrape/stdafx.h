c17641eb22a0ea77f6f4aeea31cbad64b185abad
TheHighFish <thehighfish@googlemail.com>
2016-10-29 12:15:23 +0200

    version 9.3.0

--- OpenScrape/stdafx.h
+++ OpenScrape/stdafx.h
@@ -63,8 +63,8 @@
 // ======================================================================================
 
 // Version
-#define VERSION_NUMBER			9.28
-#define VERSION_TEXT				"9.2.8"  // change version number in OpenScrape.rc also, when needed
+#define VERSION_NUMBER			9.30
+#define VERSION_TEXT				"9.3.0"  // change version number in OpenScrape.rc also, when needed
 
 #include "debug.h"
 #include "..\StructsDefines\structs_defines.h"
