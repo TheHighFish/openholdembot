18fa77e95c4dcada1b25177ac155587837ecb351
thehighfish <thehighfish@googlemail.com>
2016-12-07 12:05:31 +0100

    restoring OH

--- OpenHoldem/CSymbolEngineOpenPPLUserVariables.h
+++ OpenHoldem/CSymbolEngineOpenPPLUserVariables.h
@@ -26,11 +26,11 @@ class CSymbolEngineOpenPPLUserVariables: public CVirtualSymbolEngine {
  public:
   // Mandatory reset-functions
   void InitOnStartup();
-  void ResetOnConnection();
-  void ResetOnHandreset();
-  void ResetOnNewRound();
-  void ResetOnMyTurn();
-  void ResetOnHeartbeat();
+  void UpdateOnConnection();
+  void UpdateOnHandreset();
+  void UpdateOnNewRound();
+  void UpdateOnMyTurn();
+  void UpdateOnHeartbeat();
  public:
   void Set(CString symbol);
   bool EvaluateSymbol(const char *name, double *result, bool log = false);
