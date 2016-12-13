6ff0440765bb75d519d16ad303b41eeb2da8f587
thehighfish <thehighfish@googlemail.com>
2016-12-13 23:38:00 +0100

    linking now string-functions.DLL
    Former-commit-id: 59bf27acf674497c976790fb02ba74b6241f9f41

--- OpenHoldem/OpenHoldem.cpp
+++ OpenHoldem/OpenHoldem.cpp
@@ -38,7 +38,7 @@
 #include "Singletons.h"
 
 // !!!!! ugly temp solution as DLL does not yet work
-#include "..\StringFunctionsDLL\string_functions.cpp"
+//#include "..\StringFunctionsDLL\string_functions.cpp"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
