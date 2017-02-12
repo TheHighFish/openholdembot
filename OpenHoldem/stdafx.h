5877a69154a9622f0da0174e20634590a5cdc055
thehighfish <thehighfish@googlemail.com>
2017-02-12 12:23:13 +0100

    version 11.1.2 + RNotes

--- OpenHoldem/stdafx.h
+++ OpenHoldem/stdafx.h
@@ -134,8 +134,8 @@
 #include <string.h>
 
 // Version
-#define VERSION_NUMBER			11.11
-#define VERSION_TEXT				"11.1.1.0"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER			11.12
+#define VERSION_TEXT				"11.1.2.0"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
