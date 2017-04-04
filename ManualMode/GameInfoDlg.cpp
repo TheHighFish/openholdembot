08afb400f22f526d82d88e3a2ac897203f06a9a8
TheHighFish <thehighfish@googlemail.com>
2017-04-04 10:57:32 +0200

    2 little fixes fore MM

--- ManualMode/GameInfoDlg.cpp
+++ ManualMode/GameInfoDlg.cpp
@@ -21,21 +21,18 @@
 #include "ManualModeDlg.h"
 
 // CGameInfoDlg dialog
-CGameInfoDlg::CGameInfoDlg(CWnd* pParent /*=NULL*/)	: CDialog(CGameInfoDlg::IDD, pParent) 
-{
+CGameInfoDlg::CGameInfoDlg(CWnd* pParent /*=NULL*/)	: CDialog(CGameInfoDlg::IDD, pParent) {
 	SetUnhandledExceptionFilter(MyUnHandledExceptionFilter);
-
 	pPlacement = NULL;
 }
 
-CGameInfoDlg::~CGameInfoDlg() 
-{
-	if(pPlacement != NULL)
-		free((WINDOWPLACEMENT *) pPlacement);
+CGameInfoDlg::~CGameInfoDlg() {
+  if (pPlacement != NULL) {
+    free((WINDOWPLACEMENT *)pPlacement);
+  }
 }
 
-void CGameInfoDlg::DoDataExchange(CDataExchange* pDX) 
-{
+void CGameInfoDlg::DoDataExchange(CDataExchange* pDX) {
 	CDialog::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_SMALL_BLIND, m_SmallBlind);
 	DDX_Control(pDX, IDC_BIG_BLIND, m_BigBlind);
@@ -47,21 +44,16 @@ void CGameInfoDlg::DoDataExchange(CDataExchange* pDX)
   DDX_Control(pDX, IDC_HOLDEM_OMAHA, m_HoldemOmaha);
 }
 
-
 BEGIN_MESSAGE_MAP(CGameInfoDlg, CDialog)
 	ON_BN_CLICKED(IDOK, &CGameInfoDlg::OnBnClickedOk)
 	ON_BN_CLICKED(IDCANCEL, &CGameInfoDlg::OnBnClickedCancel)
 END_MESSAGE_MAP()
 
-
 // CGameInfoDlg message handlers
 
-BOOL CGameInfoDlg::OnInitDialog() 
-{
-	CDialog::OnInitDialog();
-
+BOOL CGameInfoDlg::OnInitDialog() {
+  CDialog::OnInitDialog();
 	CString str;
-
 	m_SmallBlind.SetWindowText(m_sblind);
 	m_BigBlind.SetWindowText(m_bblind);
 	m_BigBet.SetWindowText(m_bigbet);
@@ -69,56 +61,92 @@ BOOL CGameInfoDlg::OnInitDialog()
   m_GameType.AddString("No Limit");
 	m_GameType.AddString("Fixed Limit");
 	m_GameType.AddString("Pot Limit");
+  // Same networks as in OpenHoldem/PokerTrackerLookUp.cpp
+  // Order does not matter, as values get scraped.
 	m_Network.AddString("Stars");
 	m_Network.AddString("Party");
-	m_Network.AddString("Ultimate");
-	m_Network.AddString("Absolute");
+  m_Network.AddString("FullTilt");
+  m_Network.AddString("iPoker");
+  m_Network.AddString("Everest");
+  m_Network.AddString("Ongame");
+  m_Network.AddString("Boss");
+	m_Network.AddString("Cereus");
+  m_Network.AddString("Pacific");
+  m_Network.AddString("B2B");
 	m_Network.AddString("Microgaming");
-	m_Network.AddString("Ongame");
+  m_Network.AddString("Cake");
+  m_Network.AddString("Bodog");
+	
 	m_Network.AddString("Cryptologic");
-	m_Network.AddString("Pacific");
-	m_Network.AddString("FullTilt");
-	m_Network.AddString("B2B");
+	
+	
+	
 	m_Network.AddString("Tribeca");
 	m_Network.AddString("Worldpex");
-	m_Network.AddString("iPoker");
+	
 	m_Network.AddString("Tain");
-	m_Network.AddString("Bodog");
-	m_Network.AddString("Everest");
-	m_Network.AddString("Boss");
+	
+	
+	
 	m_Network.AddString("Betfair");
+
+ 
+
+  _pt4_siteid.insert(std::pair<CString, int>("b2b", 1000));
+  _pt4_siteid.insert(std::pair<CString, int>("microgaming", 1100));
+  _pt4_siteid.insert(std::pair<CString, int>("cake", 1200));
+  _pt4_siteid.insert(std::pair<CString, int>("bodog", 1300));
+  _pt4_siteid.insert(std::pair<CString, int>("betfair", 1400));
+  _pt4_siteid.insert(std::pair<CString, int>("cryptologic", 1500));
+  _pt4_siteid.insert(std::pair<CString, int>("ultimate", 1600));
+  _pt4_siteid.insert(std::pair<CString, int>("absolute", 1700));
+  _pt4_siteid.insert(std::pair<CString, int>("wpex", 1800));
+  _pt4_siteid.insert(std::pair<CString, int>("tribeca", 1900));
+  // 2000 not (yet) supported, whatever it is
+  _pt4_siteid.insert(std::pair<CString, int>("merge", 2100));
+  _pt4_siteid.insert(std::pair<CString, int>("winamax", 2200));
+  _pt4_siteid.insert(std::pair<CString, int>("everleaf", 2300));
+  _pt4_siteid.insert(std::pair<CString, int>("yatahay", 2400));
+  _pt4_siteid.insert(std::pair<CString, int>("enet", 2500));
+  _pt4_siteid.insert(std::pair<CString, int>("barriere", 2600));
+
+  // Yatahay???!!!!! www
 	m_Network.SelectString(0, m_network);
-
-	if (m_gametype == LIMIT_NL)
-		m_GameType.SelectString(0, "No Limit");
-	else if (m_gametype == LIMIT_PL)
-		m_GameType.SelectString(0, "Pot Limit");
-	else
-		m_GameType.SelectString(0, "Fixed Limit");
-
-	if (m_tournament)
-		m_Tournament.SetCheck(BST_CHECKED);
-	else
-		m_Tournament.SetCheck(BST_UNCHECKED);
-
-  if (m_is_omaha)
+  if (m_gametype == LIMIT_NL) {
+    m_GameType.SelectString(0, "No Limit");
+  }
+  else if (m_gametype == LIMIT_PL) {
+    m_GameType.SelectString(0, "Pot Limit");
+  }
+  else {
+    // Default
+    m_GameType.SelectString(0, "No Limit");
+  }
+  if (m_tournament) {
+    m_Tournament.SetCheck(BST_CHECKED);
+  }
+  else {
+    // Default: not a tournament
+    m_Tournament.SetCheck(BST_UNCHECKED);
+  }
+  if (m_is_omaha) {
     m_HoldemOmaha.SetCheck(BST_CHECKED);
-  else
+  }
+  else {
+    // Default: Hold'em, not Omaha
     m_HoldemOmaha.SetCheck(BST_UNCHECKED);
-
+  }
 	m_SmallBlind.SetFocus();
 	m_SmallBlind.SetSel(MAKEWORD(0, -1));
-
-	if (pPlacement != NULL) SetWindowPlacement(pPlacement);
-
+  if (pPlacement != NULL) {
+    SetWindowPlacement(pPlacement);
+  }
 	return false;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
 }
 
-void CGameInfoDlg::OnBnClickedOk()
-{
+void CGameInfoDlg::OnBnClickedOk() {
   CString str;
-
   m_SmallBlind.GetWindowText(m_sblind);
   m_BigBlind.GetWindowText(m_bblind);
   m_BigBet.GetWindowText(m_bigbet);
@@ -126,44 +154,40 @@ void CGameInfoDlg::OnBnClickedOk()
   // The constants here are not identical with the gametype constants
   // we use elsewhere. The constants used here simply depend on the order 
   // of choices in the drop-down-box.
-  if (m_GameType.GetCurSel() == 0) {
-    m_gametype = LIMIT_FL;
-  }
-  else if (m_GameType.GetCurSel() == 1) {
-    m_gametype = LIMIT_PL;
-  }
-  else if (m_GameType.GetCurSel() == 2) {
+  int choice = m_GameType.GetCurSel();
+  if (choice == 0) {
+	// No-limit is the default on top nowadays
+	// http://www.maxinmontreal.com/forums/viewtopic.php?f=156&t=20738
     m_gametype = LIMIT_NL;
   }
-  else
-  {
-    // This should not happem, but the user can edit the input
+  else if (choice == 1) {
     m_gametype = LIMIT_FL;
   }
-
-	m_Network.GetLBText(m_Network.GetCurSel(), m_network);
-
-	int network_selection = m_Network.GetCurSel();
-	if(network_selection != CB_ERR)
-	{
+  else if (choice == 2) {
+    m_gametype = LIMIT_PL;
+  }
+  else {
+    // This should not happem, but the user can edit the input
+    m_gametype = LIMIT_NL;
+  }
+  m_Network.GetLBText(m_Network.GetCurSel(), m_network);
+  int network_selection = m_Network.GetCurSel();
+	if(network_selection != CB_ERR) {
 		m_Network.GetLBText(network_selection, m_network);
 	}
-	
 	m_tournament = m_Tournament.GetCheck() & BST_CHECKED;
   m_is_omaha = m_HoldemOmaha.GetCheck() & BST_CHECKED;
-
-	if (pPlacement == NULL) 
-		pPlacement = (WINDOWPLACEMENT*) malloc(sizeof(WINDOWPLACEMENT));
+  if (pPlacement == NULL) {
+    pPlacement = (WINDOWPLACEMENT*)malloc(sizeof(WINDOWPLACEMENT));
+  }
 	GetWindowPlacement(pPlacement);
-
 	OnOK();
 }
 
-void CGameInfoDlg::OnBnClickedCancel()
-{
-	if (pPlacement == NULL) 
-		pPlacement = (WINDOWPLACEMENT*) malloc(sizeof(WINDOWPLACEMENT));
+void CGameInfoDlg::OnBnClickedCancel() {
+  if (pPlacement == NULL) {
+    pPlacement = (WINDOWPLACEMENT*)malloc(sizeof(WINDOWPLACEMENT));
+  }
 	GetWindowPlacement(pPlacement);
-
 	OnCancel();
 }
