95622364f47ea96785bea0c1310fffd778a6c1b8
TheHighFish <thehighfish@googlemail.com>
2019-04-26 13:52:06 +0200

    v13.1.2

--- OpenHoldem/stdafx.h
+++ OpenHoldem/stdafx.h
@@ -135,8 +135,8 @@
 #define OPENHOLDEM_PROGRAM
 
 // Version
-#define VERSION_NUMBER			13.11
-#define VERSION_TEXT				"13.1.1.0"  // change version number in OpenHoldem.rc also, when needed
+#define VERSION_NUMBER			13.12
+#define VERSION_TEXT				"13.1.2.0"  // change version number in OpenHoldem.rc also, when needed
 
 // PokerEval
 #include "poker_defs.h"
